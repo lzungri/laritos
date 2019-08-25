0a5cca317150c189f17ff052f7c240d49f16224f
Leandro Zungri <lzungri@gmail.com>
2019-08-25 15:40:08 -0700

    libc: Add strtoxl impl from Apple and {ctype,limits}.h from Linux

--- core/libc/string.c
+++ core/libc/string.c
@@ -27,3 +27,20 @@ int strncmp(const char *s1, const char *s2, size_t n) {
     }
     return 0;
 }
+
+size_t strnlen(const char *s, size_t maxlen) {
+    size_t size;
+    for (size = 0; *s++ != '\0' && maxlen-- > 0; size++);
+    return size;
+}
+
+char *strncpy(char *dest, const char *src, size_t n) {
+    size_t i;
+    for (i = 0; i < n && src[i] != '\0'; i++) {
+        dest[i] = src[i];
+    }
+    for (; i < n; i++) {
+        dest[i] = '\0';
+    }
+    return dest;
+}
---
+++ include/strtoxl.h
@@ -0,0 +1,4 @@
+#pragma once
+
+long strtol(const char *nptr, char **endptr, register int base);
+unsigned long strtoul(const char *nptr, char **endptr, register int base);
