9a0d9c4b5cbba3dbff229b88f823aa17d9aac2e9
Leandro Zungri <lzungri@gmail.com>
2019-09-12 12:01:59 -0700

    tools: Add ability to debug and trace a qemu instance

--- tools/qemu/launch-qemu-arm.sh
+++ tools/qemu/launch-qemu-arm.sh
@@ -4,10 +4,17 @@ SCRIPT_DIR=`dirname $0`
 source ${SCRIPT_DIR}/../../bin/include/config/auto.conf
 
 DEBUG=
-if [ "$1" == "-d" ]; then
-    echo Launching qemu in debugging mode...
+if [ "$1" == "-d" ] || [[ "$2" == "-d" ]]; then
+    echo "Launching guest in debugging mode (listening on :1234)..."
     DEBUG="-S -s"
 fi
+
+DEBUG_QEMU=
+if [ "$1" == "-D" ] || [[ "$2" == "-D" ]]; then
+    echo "Launching qemu in debugging mode (listening on :55555)..."
+    DEBUG_QEMU="gdbserver :55555"
+fi
+
 
 RAM_SIZE=$(( $CONFIG_SYS_RAM_SIZE / 1024 / 1024 ))
 
@@ -24,5 +31,26 @@ if [ -z "$CPU" ]; then
     exit 1
 fi
 
+
+echo
+qemu-system-arm --version | grep version
+echo ---------------------------------------------------------------------------
+echo
+
+TRACE_FILE=/tmp/qemu_trace
+rm -f $TRACE_FILE
+
+# Note: Configure qemu source tree with
+#   `configure --target-list=arm-softmmu,arm-linux-user --enable-debug --enable-trace-backends=simple`
 set -x
-qemu-system-arm $DEBUG -M virt -smp 4 -m ${RAM_SIZE}M -cpu $CPU -nographic -drive if=pflash,file=${SCRIPT_DIR}/../../bin/laritos.img,format=raw -d guest_errors,cpu_reset,int,unimp -D /tmp/qemu.log
+$DEBUG_QEMU qemu-system-arm --trace events=${SCRIPT_DIR}/trace_events,file=$TRACE_FILE $DEBUG -M virt -smp 4 -m ${RAM_SIZE}M -cpu $CPU -nographic -drive if=pflash,file=${SCRIPT_DIR}/../../bin/laritos.img,format=raw -d guest_errors,cpu_reset,int,unimp -D /tmp/qemu.log
+set +x
+
+echo ---------------------------------------------------------------------------
+
+
+QEMU_DIR=`dirname $(which qemu-system-arm)`
+
+echo
+echo Trace report
+$QEMU_DIR/../../../../scripts/simpletrace.py $QEMU_DIR/../trace-events-all $TRACE_FILE
