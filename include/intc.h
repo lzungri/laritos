d364cac154829365a7ad6c77957f270c2b41fe73
Leandro Zungri <lzungri@gmail.com>
2019-09-18 00:13:35 -0700

    intc: Add utility functions to enable/disable irqs

--- include/intc.h
+++ include/intc.h
@@ -40,5 +40,53 @@ typedef struct intc{
     irq_handler_info_t handlers[CONFIG_MAX_IRQS][CONFIG_MAX_HANDLERS_PER_IRQ];
 } intc_t;
 
+
+/**
+ * Helper function to enable an irq and associate it with a handler
+ */
+static inline int intc_enable_irq_with_handler(intc_t *intc, irq_t irq, irq_trigger_mode_t tmode, irq_handler_t h, void *data) {
+    if (intc->ops.set_irq_trigger_mode(intc, irq, tmode) < 0) {
+        error("Failed to set trigger mode for irq %u", irq);
+        goto error_irq_enable;
+    }
+    if (intc->ops.set_irq_enable(intc, irq, true)) {
+        error("Couldn't enable irq %u", irq);
+        goto error_irq_enable;
+    }
+    if (intc->ops.set_irq_target_cpus(intc, irq, BIT_FOR_CPU(get_cpu_id())) < 0) {
+        error("Failed to set the irq targets");
+        goto error_target;
+    }
+    if (intc->ops.set_irqs_enable_for_this_cpu(intc, true) < 0) {
+        error("Failed to enable irqs for this cpu");
+        goto error_cpu_enable;
+    }
+    if (intc->ops.add_irq_handler(intc, irq, h, data) < 0) {
+        error("Failed to add handler 0x%p for irq %u", h, irq);
+        goto error_handler;
+    }
+
+    return 0;
+
+error_handler:
+    intc->ops.set_irqs_enable_for_this_cpu(intc, false);
+error_cpu_enable:
+    intc->ops.set_irq_target_cpus(intc, irq, 0);
+error_target:
+    intc->ops.set_irq_enable(intc, irq, false);
+error_irq_enable:
+    return -1;
+}
+
+/**
+ * Helper function to disable an irq and remove its handler
+ */
+static inline int intc_disable_irq_with_handler(intc_t *intc, irq_t irq, irq_handler_t h) {
+    intc->ops.remove_irq_handler(intc, irq, h);
+    intc->ops.set_irqs_enable_for_this_cpu(intc, false);
+    intc->ops.set_irq_target_cpus(intc, irq, 0);
+    return intc->ops.set_irq_enable(intc, irq, false);
+}
+
 int intc_init(intc_t *intc, char *id, board_comp_t *bcomp,
         int (*init)(component_t *c), int (*deinit)(component_t *c));
