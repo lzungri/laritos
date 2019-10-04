01858d8d3b0f2f27ad8ed0f9007795213963d8dd
Leandro Zungri <lzungri@gmail.com>
2019-10-04 00:01:46 -0700

    core: Add time info to _laritos

--- include/core.h
+++ include/core.h
@@ -1,14 +1,20 @@
 #pragma once
 
+#include <stdbool.h>
 #include <board-types.h>
 #include <component/component.h>
 #include <driver/driver.h>
+#include <time/time.h>
 
 typedef struct {
     board_info_t bi;
     // TODO Optimize this
     component_t *components[CONFIG_COMP_MAX];
 
+    struct {
+        timezone_t tz;
+        bool dst;
+    } timeinfo;
 } laritos_t;
 
 // Global structure used as an entry point for accessing the main pieces of information
