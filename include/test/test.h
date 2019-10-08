3414b32811ac0cc9a4f653dd68247d59ae4eb6b1
Leandro Zungri <lzungri@gmail.com>
2019-10-08 00:10:39 -0700

    test: Add more assertions

--- include/test/test.h
+++ include/test/test.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <log.h>
 #include <stdint.h>
+#include <string.h>
 
 typedef struct {
     uint16_t failed;
@@ -18,6 +20,7 @@ typedef testres_t (*testfunc_t)(void);
 
 typedef struct {
     char *name;
+    char *fpath;
     testfunc_t func;
 } test_descriptor_t;
 
@@ -43,34 +46,76 @@ int test_run(test_descriptor_t *tests[]);
     static testres_t _test(void); \
     static test_descriptor_t _test_desc_ ## _test = { \
         .name = #_test, \
+        .fpath = __FILE__, \
         .func = (_test), \
     }; \
     test_descriptor_t * _testdesc_ ## _test ## _ptr __attribute__ ((section (".test"))) = &_test_desc_ ## _test; \
     static testres_t _test(void)
 
-
-#define ASSERT_EQUALS_MSG(_expr, _v, _msg, ...) \
-    __typeof__ (_expr) __exprc = (_expr); \
-    __typeof__ (_expr) __vc = (_v); \
-    if (__exprc != __vc) { \
-        error(_msg, ##__VA_ARGS__); \
-        return TEST_FAIL; \
+#define DEF_TEST_END \
+        return TEST_PASS;\
     }
 
-#define ASSERT_EQUALS(_expr, _v) \
-    ASSERT_EQUALS_MSG(_expr, _v, "Values are not equal")
+#define assert_str_equals_msg(_expr, _v, _size, _msg, ...) do { \
+        char *__exprc = (_expr); \
+        char *__vc = (_v); \
+        if (strncmp(__exprc, __vc, _size) != 0) { \
+            error("'%s' != '%s': " _msg, __exprc, __vc, ##__VA_ARGS__); \
+            return TEST_FAIL; \
+        } \
+    } while(0)
 
-#define ASSERT_TRUE_MSG(_expr, _msg, ...) \
-    if (!(_expr)) { \
-        error(_msg, ##__VA_ARGS__); \
-        return TEST_FAIL; \
-    }
+#define assert_str_equals(_expr, _v, _size) \
+    assert_str_equals_msg(_expr, _v, _size, "Strings are not equal")
 
-#define ASSERT_TRUE(_expr) \
-    ASSERT_TRUE_MSG(_expr, "Expression is not true")
+#define assert_eq_msg(_expr, _v, _msg, ...) do { \
+        __typeof__ (_expr) __exprc = (_expr); \
+        __typeof__ (_v) __vc = (_v); \
+        if (__exprc != __vc) { \
+            error(_msg, ##__VA_ARGS__); \
+            return TEST_FAIL; \
+        } \
+    } while(0)
 
-#define ASSERT_NULL_MSG(_expr, _msg, ...) \
-    ASSERT_EQUALS_MSG(_expr, NULL, _msg, ##__VA_ARGS__)
+#define assert_eq(_expr, _v) \
+    assert_eq_msg(_expr, _v, "Values are not equal")
 
-#define ASSERT_NULL(_expr) \
-    ASSERT_NULL_MSG(_expr, "Value is not NULL")
+#define assert_gt_msg(_expr, _v, _msg, ...) do { \
+        __typeof__ (_expr) __exprc = (_expr); \
+        __typeof__ (_v) __vc = (_v); \
+        if (__exprc < __vc) { \
+            error("%d should be greater than %d: "_msg, __exprc, __vc, ##__VA_ARGS__); \
+            return TEST_FAIL; \
+        } \
+    } while(0)
+
+#define assert_gt(_expr, _v) \
+    assert_gt_msg(_expr, _v, "Value is not greater")
+
+#define assert_lt_msg(_expr, _v, _msg, ...) do { \
+        __typeof__ (_expr) __exprc = (_expr); \
+        __typeof__ (_v) __vc = (_v); \
+        if (__exprc > __vc) { \
+            error("%d should be lower than %d: "_msg, __exprc, __vc, ##__VA_ARGS__); \
+            return TEST_FAIL; \
+        } \
+    } while(0)
+
+#define assert_lt(_expr, _v) \
+    assert_lt_msg(_expr, _v, "Value is not greater")
+
+#define assert_true_msg(_expr, _msg, ...) do { \
+        if (!(_expr)) { \
+            error(_msg, ##__VA_ARGS__); \
+            return TEST_FAIL; \
+        } \
+    } while(0)
+
+#define assert_true(_expr) \
+    assert_true_msg(_expr, "Expression is not true")
+
+#define assert_null_msg(_expr, _msg, ...) \
+    assert_eq_msg(_expr, NULL, _msg, ##__VA_ARGS__)
+
+#define assert_null(_expr) \
+    assert_null_msg(_expr, "Value is not NULL")
