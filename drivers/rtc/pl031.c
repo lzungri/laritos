cae16a3a0c3ba057b97cdd5371615e9be680c010
Leandro Zungri <lzungri@gmail.com>
2019-10-09 16:51:05 -0700

    rtc: Use dynamic memory

--- drivers/rtc/pl031.c
+++ drivers/rtc/pl031.c
@@ -7,14 +7,9 @@
 #include <driver/pl031.h>
 #include <utils/utils.h>
 #include <utils/math.h>
+#include <mm/heap.h>
 #include <component/timer.h>
 #include <component/component.h>
-
-#define MAX_RTCS 3
-
-// TODO Use dynamic memory instead
-static rtc_t rtcs[MAX_RTCS];
-static uint8_t cur_rtc;
 
 
 static int get_value(timer_comp_t *t, uint64_t *v) {
@@ -103,17 +98,16 @@ static int deinit(component_t *c) {
 }
 
 static int process(board_comp_t *comp) {
-    if (cur_rtc > ARRAYSIZE(rtcs)) {
-        error("Max number of rtcs components reached");
+    rtc_t *rtc = component_alloc(sizeof(rtc_t));
+    if (rtc == NULL) {
+        error("Failed to allocate memory for '%s'", comp->id);
         return -1;
     }
-
-    rtc_t *rtc = &rtcs[cur_rtc];
     timer_comp_t *t = (timer_comp_t *) rtc;
 
     if (timer_component_init(t, comp, COMP_TYPE_RTC, init, deinit) < 0) {
         error("Failed to register '%s'", comp->id);
-        return -1;
+        goto fail;
     }
     t->irq_handler = irq_handler;
     t->ops.get_value = get_value;
@@ -126,19 +120,21 @@ static int process(board_comp_t *comp) {
     board_get_ptr_attr_def(comp, "mmbase", (void **) &rtc->mm, NULL);
     if (rtc->mm == NULL) {
         error("No memory-mapped regs base was specified in the board information");
-        return -1;
+        goto fail;
     }
 
     component_set_info((component_t *) rtc, "PrimeCell RTC (pl031)", "ARM", "RTC AMBA compliant SoC");
 
     if (component_register((component_t *) rtc) < 0) {
         error("Couldn't register rtc '%s'", comp->id);
-        return -1;
+        goto fail;
     }
 
-    cur_rtc++;
-
     return 0;
+
+fail:
+    free(rtc);
+    return -1;
 }
 
 DEF_DRIVER_MANAGER(pl031, process);
