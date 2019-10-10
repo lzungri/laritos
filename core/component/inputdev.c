75379a8693a1debd9806d82c489bbcec8aaaf33d
Leandro Zungri <lzungri@gmail.com>
2019-10-09 17:12:09 -0700

    inputdev: Use dynamic memory

--- core/component/inputdev.c
+++ core/component/inputdev.c
@@ -1,41 +1,39 @@
 #include <log.h>
 #include <board-types.h>
 #include <board.h>
+#include <mm/heap.h>
 #include <component/inputdev.h>
 #include <driver/driver.h>
 
 
-#define MAX_INPUTDEVS 3
-
-// TODO Use dynamic memory instead
-static inputdev_t inputdevs[MAX_INPUTDEVS];
-static uint8_t ninputs;
-
 static int process(board_comp_t *comp) {
-    if (ninputs > ARRAYSIZE(inputdevs)) {
-        error("Max number of input components reached");
+    inputdev_t *idev = component_alloc(sizeof(inputdev_t));
+    if (idev == NULL) {
+        error("Failed to allocate memory for '%s'", comp->id);
         return -1;
     }
 
-    inputdev_t *idev = &inputdevs[ninputs];
     if (component_init((component_t *) idev, comp->id, comp, COMP_TYPE_INPUTDEV, NULL, NULL) < 0){
         error("Failed to initialize '%s'", comp->id);
-        return -1;
+        goto fail;
     }
 
     if (board_get_component_attr(comp, "transport", (component_t **) &idev->transport) < 0 ||
             idev->transport->ops.read == NULL) {
         error("No valid transport found for inputdev '%s'", comp->id);
-        return -1;
+        goto fail;
     }
 
     if (component_register((component_t *) idev) < 0) {
         error("Couldn't register inputdev '%s'", comp->id);
-        return -1;
+        goto fail;
     }
-    ninputs++;
 
     return 0;
+
+fail:
+    free(idev);
+    return -1;
 }
 
 DEF_DRIVER_MANAGER(inputdev, process);
