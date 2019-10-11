1919ea3fb7159b17b6d289492fb59b56b76e0c44
Leandro Zungri <lzungri@gmail.com>
2019-10-10 23:32:05 -0700

    cpu: Add functions to retrieve common registers

--- arch/arm/armv7-a/include/arch/cpu.h
+++ arch/arm/armv7-a/include/arch/cpu.h
@@ -3,6 +3,10 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+typedef void *regpc_t;
+typedef void *regsp_t;
+typedef void *regret_t;
+
 /**
  * The DFSR holds status information about the last data fault
  */
@@ -248,4 +252,30 @@ static inline const psr_t get_spsr(void) {
     psr_t spsr;
     asm("mrs %0, spsr" : "=r" (spsr.v));
     return spsr;
+}
+
+/**
+ * Note: __attribute__((always_inline)) so that this function is always expanded and thus
+ * we get a useful PC, not just the PC inside the arch_regs_get_pc() function (in case it
+ * wasn't expanded by the compiler)
+ *
+ * @return: Current program counter
+ */
+__attribute__((always_inline)) static inline regpc_t arch_regs_get_pc(void) {
+    regpc_t pc;
+    asm("mov %0, pc" : "=r" (pc));
+    return (char *) pc - 4;
+}
+
+/**
+ * Note: __attribute__((always_inline)) so that this function is always expanded and thus
+ * we get a useful return value, not just the return address of the arch_regs_get_pc() function (in case it
+ * wasn't expanded by the compiler)
+ *
+ * @return: Function return address
+ */
+__attribute__((always_inline)) static inline regret_t arch_regs_get_retaddr(void) {
+    regret_t ret;
+    asm("mov %0, lr" : "=r" (ret));
+    return ret;
 }
--- include/cpu.h
+++ include/cpu.h
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include <core.h>
+#include <arch/cpu.h>
 #include <component/cpu.h>
 #include <component/component.h>
 
@@ -24,4 +25,26 @@ static inline cpu_t *cpu(void) {
         }
     }
     return NULL;
+}
+
+/**
+ * Note: __attribute__((always_inline)) so that this function is always expanded and thus
+ * we get a useful PC, not just the PC inside the regs_get_pc() function (in case it
+ * wasn't expanded by the compiler)
+ *
+ * @return Current program counter
+ */
+__attribute__((always_inline)) static inline regpc_t regs_get_pc(void) {
+    return arch_regs_get_pc();
+}
+
+/**
+ * Note: __attribute__((always_inline)) so that this function is always expanded and thus
+ * we get a useful return value, not just the return address of the regs_get_pc() function (in case it
+ * wasn't expanded by the compiler)
+ *
+ * @return: Function return address
+ */
+__attribute__((always_inline)) static inline regpc_t regs_get_retaddr(void) {
+    return arch_regs_get_retaddr();
 }
