6505b05ba20443da27e791e874b0cfb4f4c5dad7
Leandro Zungri <lzungri@gmail.com>
2019-10-18 01:34:13 -0700

    loader: Clean up code

--- core/loader/loader.c
+++ core/loader/loader.c
@@ -5,8 +5,7 @@
 #include <loader/loader.h>
 
 int loader_load_app(uint16_t appidx) {
-    appheader_t *apph = (appheader_t *)__apps_start;
-    info("App magic: 0x%lX", apph->magic);
+    appheader_t *apph = (appheader_t *) __apps_start;
 
     if (apph->magic != APPMAGIC) {
         error("No app #%u or invalid magic number", appidx);
@@ -14,7 +13,5 @@ int loader_load_app(uint16_t appidx) {
     }
 
     int (*appmain)(void) = (int (*)(void)) (__apps_start + (uint32_t) apph->text_start);
-//    asm("bl %0" : : "I" (appmain));
-    appmain();
-    return 0;
+    return appmain();
 }
