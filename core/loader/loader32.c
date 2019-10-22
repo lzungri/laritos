80b0b3fd230c4ff56a1b6bb0e4ce73b5bc802142
Leandro Zungri <lzungri@gmail.com>
2019-10-22 13:24:52 -0700

    loader: Initial implementation of the relocations

--- core/loader/loader32.c
+++ core/loader/loader32.c
@@ -5,6 +5,8 @@
 #include <mm/heap.h>
 #include <userspace/app.h>
 #include <loader/loader.h>
+#include <loader/elf.h>
+#include <arch/loader.h>
 
 static inline void *allocate_app_image(appheader_t *apph) {
     size_t imgsize = apph->text_size + apph->data_size + apph->bss_size +
@@ -35,11 +37,31 @@ static int load_image_from_memory(appheader_t *apph, void *addr) {
     return 0;
 }
 
-static int setup_image_context(appheader_t *apph, void *addr) {
+static int setup_image_context(void *addr) {
+    appheader_t *apph = addr;
     asm("mov r9, %0" : : "r" ((char *) addr + apph->got_start - 0x30));
 
     uint32_t *ptr = (uint32_t *) ((char *) addr + apph->got_start + 0x0);
     *ptr = (uint32_t) ((char *) addr + 0xf4);
+    return 0;
+}
+
+static int relocate_image(void *addr) {
+    appheader_t *apph = addr;
+    int i;
+    uint32_t nrels = apph->reloc_size / sizeof(Elf32_Rel);
+    for (i = 0; i < nrels; i++) {
+        const Elf32_Rel *rel = (Elf32_Rel *)
+                ((char *) addr + apph->reloc_start + i * sizeof(Elf32_Rel));
+        const Elf32_Sym *sym = (Elf32_Sym *)
+                ((char *) addr + apph->symbol_start + ELF32_R_SYM(rel->r_info) * sizeof(Elf32_Sym));
+
+        verbose("Relocating symbol #%lu, type=%lu", ELF32_R_SYM(rel->r_info), ELF32_R_TYPE(rel->r_info));
+        if (arch_relocate_symbol(addr, rel, sym) < 0) {
+            error("Failed to relocate symbol #%lu, type=%lu", ELF32_R_SYM(rel->r_info), ELF32_R_TYPE(rel->r_info));
+            return -1;
+        }
+    }
     return 0;
 }
 
@@ -48,6 +70,11 @@ int loader_load_app(uint16_t appidx) {
 
     if (apph->magic != APPMAGIC) {
         error("No app #%u or invalid magic number", appidx);
+        return -1;
+    }
+
+    if (apph->hdrver != HEADER_VERSION) {
+        error("Invalid version for app #%u", appidx);
         return -1;
     }
 
@@ -62,14 +89,20 @@ int loader_load_app(uint16_t appidx) {
         goto error_load;
     }
 
-    if (setup_image_context(apph, imgaddr) < 0) {
+    if (setup_image_context(imgaddr) < 0) {
         error("Failed to setup context for app #%u", appidx);
         goto error_setup;
+    }
+
+    if (relocate_image(imgaddr) < 0) {
+        error("Failed to relocate app #%u", appidx);
+        goto error_reloc;
     }
 
     int (*main)(void) = (int (*)(void)) (sizeof(appheader_t) + (char *) imgaddr);
     return main();
 
+error_reloc:
 error_setup:
 error_load:
     free(imgaddr);
