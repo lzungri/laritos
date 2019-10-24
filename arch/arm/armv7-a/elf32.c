a2712a40ccceebd0681d4cbff239f47ba283cee4
Leandro Zungri <lzungri@gmail.com>
2019-10-23 19:57:15 -0700

    arm: Implement R_ARM_GOT_BREL12 relocation

--- arch/arm/armv7-a/elf32.c
+++ arch/arm/armv7-a/elf32.c
@@ -6,11 +6,12 @@
 #include <utils/math.h>
 
 /**
- * Relocate symbol on image at address <imgaddr>
+ * Relocates symbol on image at address <imgaddr>
  *
  * NOTE: Thumb mode not supported
  */
-int arch_elf32_relocate_symbol(void *imgaddr, const Elf32_Rel *rel, const Elf32_Sym *sym) {
+int arch_elf32_relocate_symbol(void *imgaddr, const Elf32_Rel *rel, const Elf32_Sym *sym, uint32_t *got) {
+    char *imgbase = imgaddr;
 
     /**
      * From ARM IHI0044F_aaelf.pdf document.
@@ -31,26 +32,34 @@ int arch_elf32_relocate_symbol(void *imgaddr, const Elf32_Rel *rel, const Elf32_
      * 	  X is the 32-bit result of normal relocation processing
      */
     switch (ELF32_R_TYPE(rel->r_info)) {
-    case R_ARM_CALL:;
+    case R_ARM_CALL:
+        // Ignore this type of relocation, they are already resolved
+        break;
+
         /**
          * Instruction | REL Addend (A)
          * BL, BLX     | sign_extend (insn[23:0] << 2)
          *
          * X = ((S + A) | T) – P
+         * X = X & 0x03FFFFFE
          */
         uint32_t S = sym->st_value;
-        uint32_t *P = (uint32_t *) ((char *) imgaddr + rel->r_offset);
+        uint32_t *P = (uint32_t *) (imgbase + rel->r_offset);
         int32_t A = sign_extend_32(*P & 0x00FFFFFF, 24) << 2;
 
         int32_t X = (S + A) - (uint32_t) P;
-        X >>= 2;
-        X &= 0x00FFFFFF;
+        X = (X & 0x03FFFFFE) >> 2;
 
         *P = (*P & 0xFF000000) | (X & 0x00FFFFFF);
         break;
+    case R_ARM_GOT32:
+    case R_ARM_GOT_BREL12:;
+        uint32_t gotidx = *((uint32_t *) (imgbase + rel->r_offset)) / 4;
+        got[gotidx] = (uint32_t) (imgbase + sym->st_value);
+        break;
     default:
         error("Unknown relocation type=0x%lX at offset=0x%lX for symbol value=0x%lX", ELF32_R_TYPE(rel->r_info), rel->r_offset, sym->st_value);
-//        return -1;
+        return -1;
     }
 
     return 0;
--- arch/arm/armv7-a/include/arch/elf32.h
+++ arch/arm/armv7-a/include/arch/elf32.h
@@ -7,4 +7,4 @@ static inline bool arch_elf32_is_supported(Elf32_Ehdr *elf) {
     return elf->e_machine == EM_ARM;
 }
 
-int arch_elf32_relocate_symbol(void *imgaddr, const Elf32_Rel *rel, const Elf32_Sym *sym);
+int arch_elf32_relocate_symbol(void *imgaddr, const Elf32_Rel *rel, const Elf32_Sym *sym, uint32_t *got);
