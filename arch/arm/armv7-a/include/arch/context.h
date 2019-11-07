d8be959f36ca0c27116cbbcb36530008a1ee654c
Leandro Zungri <lzungri@gmail.com>
2019-11-06 18:59:26 -0800

    context: Add comments

--- arch/arm/armv7-a/include/arch/context.h
+++ arch/arm/armv7-a/include/arch/context.h
@@ -7,12 +7,18 @@
 static inline void arch_restore_context(pcb_t *pcb) {
     // volatile to prevent any gcc optimization on the assembly code
     asm volatile (
-        "mov sp, %0               \n"
-        "ldmfd sp!, {r0, lr}      \n"
-        "msr spsr_cxsf, r0        \n"
-        "ldmfd sp!, {r0-r13}^     \n" /* ^ to save the registers in the target processor mode (not the current bank)*/
-        "subs pc, lr, #0          \n"
+        /* Restore stack pointer from pcb */
+        "mov sp, %0            \n"
+        /* Pop the SPSR and link register */
+        "ldmfd sp!, {r0, lr}   \n"
+        /* Update the SPSR (CPSR will be restored from this value)*/
+        "msr spsr_cxsf, r0     \n"
+        /* ^ to save the registers in the target processor mode (not the current bank)*/
+        "ldmfd sp!, {r0-r13}^  \n"
+        /* subS to also restore cpsr from spsr */
+        "subs pc, lr, #0       \n"
         :
         : "r" (pcb->mm.sp)
-        : "memory" /* Memory barrier (do not reorder read/writes)*/ , "cc" /* Tell gcc this code modifies the cpsr */);
+        : "memory" /* Memory barrier (do not reorder read/writes)*/ ,
+          "cc" /* Tell gcc this code modifies the cpsr */);
 }
