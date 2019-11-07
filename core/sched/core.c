65911296a9e99d8b0f86c58ccb232a3ca126cf70
Leandro Zungri <lzungri@gmail.com>
2019-11-06 19:07:21 -0800

    sched: Only move to ready on preemption

--- core/sched/core.c
+++ core/sched/core.c
@@ -3,6 +3,7 @@
 #include <process/pcb.h>
 #include <sched/core.h>
 #include <sched/context.h>
+#include <utils/assert.h>
 
 void switch_to(pcb_t *pcb) {
     sched_move_to_running(pcb);
@@ -12,7 +13,9 @@ void switch_to(pcb_t *pcb) {
 void context_switch(pcb_t *cur, pcb_t *to) {
     if (cur != NULL && cur != to) {
         pcb_set_current(NULL);
-        sched_move_to_ready(cur);
+        if (cur->sched.status == PCB_STATUS_RUNNING) {
+            sched_move_to_ready(cur);
+        }
     }
     switch_to(to);
 }
@@ -20,5 +23,8 @@ void context_switch(pcb_t *cur, pcb_t *to) {
 void schedule(void) {
     pcb_t *curpcb = pcb_get_current();
     pcb_t *pcb = sched_algo_pick_ready(curpcb);
+
+    assert(pcb != NULL, "No process ready for execution, where is the idle process?");
+
     return context_switch(curpcb, pcb);
 }
