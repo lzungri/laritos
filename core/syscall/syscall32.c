9c015ebd40272a5a7f12ea02b881b1534df0b72b
Leandro Zungri <lzungri@gmail.com>
2019-11-07 16:59:47 -0800

    syscall: Implement yield syscall (just for testing)

--- core/syscall/syscall32.c
+++ core/syscall/syscall32.c
@@ -1,21 +1,29 @@
 #define DEBUG
 #include <log.h>
 
-#include <syscall/syscall.h>
 #include <cpu.h>
 #include <process/pcb.h>
-
 #include <sched/core.h>
+#include <syscall/syscall.h>
+#include <utils/assert.h>
 
 int syscall(int sysno, regsp_t sp, int32_t arg0, int32_t arg1, int32_t arg2, int32_t arg3, int32_t arg4, int32_t arg5) {
     pcb_set_current_pcb_stack(sp);
     verbose_async("syscall_%d(%lx, %lx, %lx, %lx, %lx, %lx)", sysno, arg0, arg1, arg2, arg3, arg4, arg5);
 
     pcb_t *pcb = pcb_get_current();
-    if (sysno == 0) {
+    assert(pcb != NULL, "pcb_get_current() cannot be NULL on system call");
+
+    switch (sysno) {
+    case 0:
         info_async("Killing process pid=%u", pcb->pid);
         sched_move_to_zombie(pcb);
         schedule();
+        break;
+    case 1:
+        info_async("Yield process pid=%u", pcb->pid);
+        schedule();
+        break;
     }
     return 0;
 }
