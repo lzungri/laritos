cd3fef639df4aa801034acf0bc16e6dc5b369602
Leandro Zungri <lzungri@gmail.com>
2019-11-10 23:25:12 -0800

    context: Add documentation

--- arch/arm/armv7-a/include/arch/context.h
+++ arch/arm/armv7-a/include/arch/context.h
@@ -35,24 +35,29 @@ static inline void arch_context_init(struct pcb *pcb, void *retaddr, cpu_mode_t 
 static inline void arch_context_restore(pcb_t *pcb) {
     regpsr_t *psr = (regpsr_t *) pcb->mm.sp;
 
+    // The context restore will change based on whether we need to restore an svc
+    // or user context. Check this by inspecting the psr value saved in the stack
     if (psr->b.mode == ARM_CPU_MODE_SUPERVISOR) {
         regsp_t cursp = pcb->mm.sp;
-        // Restore previous sp
+        // Restore previous sp (it was saved in r0 in arch_context_save_and_restore())
         pcb->mm.sp = (regsp_t) ((spregs_t *) pcb->mm.sp)->r[0];
 
         // volatile to prevent any gcc optimization on the assembly code
         asm volatile (
             /* Restore stack pointer from pcb */
             "mov sp, %0            \n"
-            /* Pop the SPSR and link register */
+            /* Pop the SPSR and return address */
             "ldmfd sp!, {r0, lr}   \n"
             /* Update the SPSR (CPSR will be restored from this value)*/
             "msr spsr_cxsf, r0     \n"
+            /* Restore all the other registers */
             "ldmfd sp!, {r0-r12}   \n"
-            /* Don't care if we lose r0 original value */
+            /* Save the return address in r0, (don't care if we lose r0 original value) */
             "mov r0, lr            \n"
-            "ldmfd sp!, {lr}   \n"
-            /* subS to also restore cpsr from spsr */
+            /* Get the original lr value from the saved context */
+            "ldmfd sp!, {lr}       \n"
+            /* Return to the address specified by the retaddr in the saved context.
+             * subS also restores cpsr from spsr */
             "subs pc, r0, #0       \n"
             :
             : "r" (cursp)
@@ -66,17 +71,18 @@ static inline void arch_context_restore(pcb_t *pcb) {
     // volatile to prevent any gcc optimization on the assembly code
     asm volatile (
         /* Restore stack pointer from pcb */
-        "mov sp, %0            \n"
+        "mov sp, %0               \n"
         /* Pop the SPSR and link register */
-        "ldmfd sp!, {r0, lr}   \n"
+        "ldmfd sp!, {r0, lr}      \n"
         /* Update the SPSR (CPSR will be restored from this value)*/
-        "msr spsr_cxsf, r0     \n"
+        "msr spsr_cxsf, r0        \n"
         /* ^ to save the registers in the user bank (not the svc bank)*/
         "ldmfd sp!, {r0-r12, lr}^ \n"
-        "stmdb sp!, {sp}   \n"
-        "ldmfd sp!, {sp}^  \n"
+        /* Trick to save the sp_svc into sp_user */
+        "stmdb sp!, {sp}          \n"
+        "ldmfd sp!, {sp}^         \n"
         /* subS to also restore cpsr from spsr */
-        "subs pc, lr, #0       \n"
+        "subs pc, lr, #0          \n"
         :
         : "r" (pcb->mm.sp)
         : "memory", /* Memory barrier (do not reorder read/writes)*/
@@ -88,8 +94,10 @@ static inline void arch_context_save_and_restore(pcb_t *spcb, pcb_t *rpcb) {
     bool ctx_saved = false;
     // volatile to prevent any gcc optimization on the assembly code
     asm volatile (
-        /* Save in r0 the previous spcb->mm.sp */
+        /* Save in r0 the previous spcb->mm.sp. We will use this value to restore the
+         * previous pcb->mm.sp once this context is restored, see arch_context_restore()*/
         "mov r0, %1              \n"
+        /* Push registers into sp_svc stack (remember that svc code uses the process stack) */
         "stmfd sp!, {r0-r12, lr} \n"
         /* Save current PSR */
         "mrs r0, cpsr_all        \n"
@@ -99,16 +107,21 @@ static inline void arch_context_save_and_restore(pcb_t *spcb, pcb_t *rpcb) {
         "stmfd sp!, {r0, r1}     \n"
         /* ctx_saved = true; */
         "mov %0, #1              \n"
+        /* Skip the following instruction, that one will only be executed once the context is
+         * restored */
         "b 1f                    \n"
+        /* ctx_saved = true; */
         "mov %0, #0              \n"
      "1:                         \n"
         : "=&r" (ctx_saved)
         : "r" (spcb->mm.sp)
         : "memory",  /* Memory barrier (do not reorder read/writes) */
           "cc", /* Tell gcc this code modifies the cpsr */
-          "r0", "r1", "r2" /* Do not use r0-r2 in compiler generated code since we are using them */);
+          "r0", "r1" /* Do not use r0-r1 in compiler generated code since we are using them */);
 
+    // Check whether this is a context save or returning from a context restore
     if (ctx_saved) {
+        // Update the context pointer
         spcb->mm.sp = arch_regs_get_sp();
 
         // Since we screw the stack we need to do the saving and restoring here, that way,
