8e47fab0c75e396c7cb7b6e3e88013d122931911
Leandro Zungri <lzungri@gmail.com>
2019-11-20 15:57:45 -0800

    tick: Add type for absolute tick values

--- include/timer/tick.h
+++ include/timer/tick.h
@@ -2,4 +2,12 @@
 
 #include <stdint.h>
 
+/**
+ * Type representing a relative amount of ticks
+ */
 typedef uint32_t tick_t;
+
+/**
+ * Type used to deal with absolute tick values, e.g. uptime
+ */
+typedef uint64_t abstick_t;
