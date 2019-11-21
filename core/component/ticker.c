ff2e2aa959b4fdb4bc1ae791a6de3196059c9002
Leandro Zungri <lzungri@gmail.com>
2019-11-20 16:01:27 -0800

    ticker: Add callback list and cb management functions

--- core/component/ticker.c
+++ core/component/ticker.c
@@ -8,16 +8,26 @@
 #include <component/component.h>
 #include <timer/tick.h>
 #include <utils/math.h>
+#include <dstruct/list.h>
+#include <mm/heap.h>
 
 
 static int ticker_cb(timer_comp_t *t, void *data) {
-    verbose_async("Tick");
+    ticker_comp_t *ticker = (ticker_comp_t *) data;
+    ticker_cb_info_t *ti;
+    list_for_each_entry(ti, &ticker->cbs, list) {
+        verbose_async("Executing ticker callback 0x%p(data=0x%p)", ti->cb, ti->data);
+        if (ti->cb(ticker, ti->data) < 0) {
+            error_async("Failed to execute callback 0x%p(data=0x%p)", ti->cb, ti->data);
+        }
+    }
 
-    // Call CBs
     return 0;
 }
 
 int ticker_init(ticker_comp_t *t) {
+    INIT_LIST_HEAD(&t->cbs);
+
     tick_t timer_ticks = t->timer->curfreq / t->ticks_per_sec;
     if (timer_ticks == 0) {
         // If the timer resolution cannot handle the required ticks_per_sec, then
@@ -34,10 +44,33 @@ int ticker_deinit(ticker_comp_t *t) {
 }
 
 static int add_callback(ticker_comp_t *t, ticker_cb_t cb, void *data) {
+    verbose_async("Adding ticker callback 0x%p(data=0x%p)", cb, data);
 
-    // TODO Maybe we can just hardcode the callbacks
+    ticker_cb_info_t *ti = calloc(1, sizeof(ticker_cb_info_t));
+    if (ti == NULL) {
+        error("Couldn't allocate memory for ticker_cb_info_t");
+        return -1;
+    }
 
+    ti->cb = cb;
+    ti->data = data;
+    INIT_LIST_HEAD(&ti->list);
 
+    list_add_tail(&ti->list, &t->cbs);
+    return 0;
+}
+
+static int remove_callback(ticker_comp_t *t, ticker_cb_t cb, void *data) {
+    ticker_cb_info_t *pos;
+    ticker_cb_info_t *tmp;
+    list_for_each_entry_safe(pos, tmp, &t->cbs, list) {
+        if (pos->cb == cb && pos->data == data) {
+            verbose_async("Removing ticker callback 0x%p(data=0x%p)", cb, data);
+            list_del(&pos->list);
+            free(pos);
+            break;
+        }
+    }
     return 0;
 }
 
@@ -50,6 +83,7 @@ int ticker_component_init(ticker_comp_t *t, board_comp_t *bcomp,
     }
 
     t->ops.add_callback = add_callback;
+    t->ops.remove_callback = remove_callback;
 
     if (board_get_component_attr(bcomp, "timer", (component_t **) &t->timer) < 0) {
         error("Invalid or no timer component specified in the board info");
