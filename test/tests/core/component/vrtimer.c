a86e0797dcd176fa2fd5fae07d546a5bd51ef893
Leandro Zungri <lzungri@gmail.com>
2019-11-22 19:30:07 -0800

    vrtimer: Do not use ticker in vrtimer tests

--- test/tests/core/component/vrtimer.c
+++ test/tests/core/component/vrtimer.c
@@ -28,6 +28,12 @@ static bool is_vrtimer_registered(vrtimer_comp_t *t, vrtimer_cb_t cb, void *data
     return false;
 }
 
+static inline abstick_t get_timer_cur_value(vrtimer_comp_t *t) {
+    abstick_t cur;
+    t->hrtimer->ops.get_value(t->hrtimer, &cur);
+    return cur;
+}
+
 static int cb0(vrtimer_comp_t *t, void *data) {
     return 0;
 }
@@ -36,7 +42,7 @@ T(vrtimer_can_add_and_remove_timers) {
     vrtimer_comp_t *t = get_vrtimer();
     tassert(t != NULL);
 
-    t->ops.add_vrtimer(t, 0, cb0, NULL, true);
+    t->ops.add_vrtimer(t, 10000000, cb0, NULL, true);
     tassert(is_vrtimer_registered(t, cb0, NULL, true));
 
     // Try to remove the wrong timer
@@ -52,8 +58,7 @@ T(vrtimer_can_add_and_remove_timers) {
 TEND
 
 static int cb1(vrtimer_comp_t *t, void *data) {
-    abstick_t *ticks = (abstick_t *) data;
-    *ticks = _laritos.timeinfo.ticks;
+    t->hrtimer->ops.get_value(t->hrtimer, (abstick_t *) data);
     return 0;
 }
 
@@ -62,13 +67,12 @@ T(vrtimer_callback_is_called_after_n_ticks) {
     tassert(t != NULL);
 
     tick_t n = 3;
-    abstick_t saved_ticks = _laritos.timeinfo.ticks;
     abstick_t cb_ticks = 0;
+    abstick_t saved_ticks = get_timer_cur_value(t);
 
     t->ops.add_vrtimer(t, n, cb1, &cb_ticks, false);
-    tassert(is_vrtimer_registered(t, cb1, &cb_ticks, false));
 
-    while(cb_ticks == 0 && _laritos.timeinfo.ticks < saved_ticks + 2 * n);
+    while(cb_ticks == 0 && get_timer_cur_value(t) < saved_ticks + 2 * n);
 
     tassert(cb_ticks >= saved_ticks + n);
 
@@ -83,10 +87,10 @@ static int cb2(vrtimer_comp_t *t, void *data) {
     if (cb2_ticks == 0) {
         // Initialize boolean used for validation
         *valid_tick = true;
-    } else if (_laritos.timeinfo.ticks != cb2_ticks + cb2_n) {
+    } else if (get_timer_cur_value(t) != cb2_ticks + cb2_n) {
         *valid_tick = false;
     }
-    cb2_ticks = _laritos.timeinfo.ticks;
+    cb2_ticks = get_timer_cur_value(t);
     return 0;
 }
 
@@ -94,13 +98,13 @@ T(vrtimer_callbacks_are_called_on_every_n_ticks_if_vrtimer_is_periodic) {
     vrtimer_comp_t *t = get_vrtimer();
     tassert(t != NULL);
 
-    abstick_t deadline = _laritos.timeinfo.ticks + 3 * cb2_n;
+    abstick_t deadline = get_timer_cur_value(t) + 4 * cb2_n;
     bool valid_tick = false;
 
     t->ops.add_vrtimer(t, cb2_n, cb2, &valid_tick, true);
     tassert(is_vrtimer_registered(t, cb2, &valid_tick, true));
 
-    while(_laritos.timeinfo.ticks < deadline);
+    while(get_timer_cur_value(t) < deadline);
 
     tassert(valid_tick);
 
