841fd586afd55d3675966ec6d9fceee45d8bcec5
Leandro Zungri <lzungri@gmail.com>
2019-12-01 01:38:04 -0800

    cpu: Add sched reference in cpu_t

--- include/component/cpu.h
+++ include/component/cpu.h
@@ -10,11 +10,13 @@ typedef struct {
     int (*set_irqs_enable)(struct cpu *c, bool enabled);
 } cpu_ops_t;
 
+struct sched_comp;
 typedef struct cpu {
     component_t parent;
 
     uint8_t id;
     struct intc *intc;
+    struct sched_comp *sched;
 
     cpu_ops_t ops;
 } cpu_t;
