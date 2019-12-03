8734a3ec41e70664083b4ee6e9036051c7c4bf75
Leandro Zungri <lzungri@gmail.com>
2019-12-03 01:24:49 -0800

    rr: Only move to ready when the process is currently running

--- core/sched/preempt-rr.c
+++ core/sched/preempt-rr.c
@@ -19,7 +19,10 @@ static inline pcb_t *pick_ready(sched_comp_t *sched, struct cpu *cpu, pcb_t *cur
 
 static int rr_ticker_cb(ticker_comp_t *t, void *data) {
     if (_laritos.process_mode) {
-        sched_move_to_ready(pcb_get_current());
+        pcb_t *cur = pcb_get_current();
+        if (cur->sched.status == PCB_STATUS_RUNNING) {
+            sched_move_to_ready(cur);
+        }
         _laritos.sched.need_sched = true;
     }
     return 0;
