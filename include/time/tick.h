3f4159ac15d16dacdff78c08a1e193b2ac04c80c
Leandro Zungri <lzungri@gmail.com>
2019-12-25 17:41:07 -0800

    tick: Add macros to translate tick <-> time

--- include/time/tick.h
+++ include/time/tick.h
@@ -16,3 +16,7 @@ typedef uint64_t abstick_t;
 #define SEC_TO_TICK(_timer, _secs) ((_secs) * (_timer)->curfreq)
 #define MS_TO_TICK(_timer, _ms)  (((_ms) * (_timer)->curfreq) / MSEC_PER_SEC)
 #define US_TO_TICK(_timer, _us)  (((_us) * (_timer)->curfreq) / USEC_PER_SEC)
+
+#define TICK_TO_SEC(_timer, _ticks) ((_ticks) / (_timer)->curfreq)
+#define TICK_TO_MS(_timer, _ticks) (((_ticks) * MSEC_PER_SEC) / (_timer)->curfreq)
+#define TICK_TO_US(_timer, _ticks) (((_ticks) * USEC_PER_SEC) / (_timer)->curfreq)
