b6ca5d645565d6a855b458cb4e9fad0dcf87787a
Leandro Zungri <lzungri@gmail.com>
2019-12-30 00:58:37 -0800

    condition: Add functions to handle conditions with proclock taken

--- core/sync/condition.c
+++ core/sync/condition.c
@@ -13,30 +13,41 @@ int condition_init(condition_t *cond) {
     return 0;
 }
 
-static void condition_block_current_process(condition_t *cond) {
+static void condition_block_current_process(condition_t *cond, bool proclocked) {
     pcb_t *pcb = process_get_current();
 
     assert(list_empty(&pcb->sched.blockedlst), "A process can only be waiting for max 1 event");
-    // TODO Order list by process priority
     list_add_tail(&pcb->sched.blockedlst, &cond->blocked);
 
-    irqctx_t ctx2;
-    spinlock_acquire(&_laritos.proclock, &ctx2);
-    sched_move_to_blocked_locked(pcb);
-    spinlock_release(&_laritos.proclock, &ctx2);
+    if (proclocked) {
+        sched_move_to_blocked_locked(pcb);
+    } else {
+        irqctx_t ctx2;
+        spinlock_acquire(&_laritos.proclock, &ctx2);
+        sched_move_to_blocked_locked(pcb);
+        spinlock_release(&_laritos.proclock, &ctx2);
+    }
 
     verbose_async("pid=%u waiting for condition=0x%p", pcb->pid, cond);
 }
 
 void condition_wait_locked(condition_t *cond, spinlock_t *spin, irqctx_t *ctx) {
-    condition_block_current_process(cond);
+    condition_block_current_process(cond, false);
 
     spinlock_release(spin, ctx);
     schedule();
     spinlock_acquire(spin, ctx);
 }
 
-static inline void wakeup_pcb(pcb_t *pcb, condition_t *cond) {
+void condition_wait_proclocked(condition_t *cond, irqctx_t *ctx) {
+    condition_block_current_process(cond, true);
+
+    spinlock_release(&_laritos.proclock, ctx);
+    schedule();
+    spinlock_acquire(&_laritos.proclock, ctx);
+}
+
+static inline void wakeup_pcb(pcb_t *pcb, condition_t *cond, bool proclocked) {
     if (pcb == NULL) {
         return;
     }
@@ -44,25 +55,46 @@ static inline void wakeup_pcb(pcb_t *pcb, condition_t *cond) {
     list_del_init(&pcb->sched.blockedlst);
 
     verbose_async("Waking up pid=%u waiting for condition=0x%p", pcb->pid, cond);
-    irqctx_t ctx2;
-    spinlock_acquire(&_laritos.proclock, &ctx2);
-    sched_move_to_ready_locked(pcb);
-    spinlock_release(&_laritos.proclock, &ctx2);
+
+    if (proclocked) {
+        sched_move_to_ready_locked(pcb);
+    } else {
+        irqctx_t ctx2;
+        spinlock_acquire(&_laritos.proclock, &ctx2);
+        sched_move_to_ready_locked(pcb);
+        spinlock_release(&_laritos.proclock, &ctx2);
+    }
 }
 
-pcb_t *condition_notify_locked(condition_t *cond) {
+static inline pcb_t *notify_locked(condition_t *cond, bool proclocked) {
     pcb_t *pcb = list_first_entry_or_null(&cond->blocked, pcb_t, sched.blockedlst);
-    wakeup_pcb(pcb, cond);
+    wakeup_pcb(pcb, cond, proclocked);
     return pcb;
 }
 
-bool condition_notify_all_locked(condition_t *cond) {
+pcb_t *condition_notify_proclocked(condition_t *cond) {
+    return notify_locked(cond, true);
+}
+
+pcb_t *condition_notify_locked(condition_t *cond) {
+    return notify_locked(cond, false);
+}
+
+bool notify_all_locked(condition_t *cond, bool proclocked) {
     pcb_t *pcb;
     pcb_t *tmp;
     bool proc_awakened = false;
     list_for_each_entry_safe(pcb, tmp, &cond->blocked, sched.blockedlst) {
-        wakeup_pcb(pcb, cond);
+        wakeup_pcb(pcb, cond, proclocked);
         proc_awakened = true;
     }
     return proc_awakened;
+}
+
+bool condition_notify_all_proclocked(condition_t *cond) {
+    return notify_all_locked(cond, true);
+}
+
+bool condition_notify_all_locked(condition_t *cond) {
+    return notify_all_locked(cond, false);
 }
