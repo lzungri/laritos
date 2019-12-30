517d0754c1a51f2b00becef1754a89fdaf981db8
Leandro Zungri <lzungri@gmail.com>
2019-12-29 17:03:21 -0800

    semaphore: Add test case for notify all method

--- test/tests/core/sync/semaphore.c
+++ test/tests/core/sync/semaphore.c
@@ -100,7 +100,7 @@ T(semaphore_012_sequence) {
                         8196, process_get_current()->sched.priority - 3);
     tassert(p2 != NULL);
 
-    sleep(3);
+    schedule();
 
     tassert(is_process_in(&p0->sched.blockedlst, &sems_012[0].cond.blocked));
     tassert(is_process_in(&p1->sched.blockedlst, &sems_012[1].cond.blocked));
@@ -111,4 +111,50 @@ T(semaphore_012_sequence) {
 
     sem_release(&sems_012[0]);
     sem_acquire(&sems_012[3]);
+TEND
+
+static int proc_acquire(void *data) {
+    sem_t *sem = (sem_t *) data;
+    sem_acquire(sem);
+    debug("Semaphore acquired");
+    return 0;
+}
+
+T(semaphore_blocked_proc_with_highest_priority_acquires_semaphore_after_releasing_an_instance) {
+    sem_t sem;
+    sem_init(&sem, 0);
+
+    pcb_t *p0 = process_spawn_kernel_process("proc0", proc_acquire, &sem,
+                        8196, process_get_current()->sched.priority - 1);
+    tassert(p0 != NULL);
+    schedule();
+
+    pcb_t *p1 = process_spawn_kernel_process("proc1", proc_acquire, &sem,
+                        8196, process_get_current()->sched.priority - 2);
+    tassert(p1 != NULL);
+    schedule();
+
+    pcb_t *p2 = process_spawn_kernel_process("proc2", proc_acquire, &sem,
+                        8196, process_get_current()->sched.priority - 3);
+    tassert(p2 != NULL);
+    schedule();
+
+    tassert(is_process_in(&p0->sched.blockedlst, &sem.cond.blocked));
+    tassert(is_process_in(&p1->sched.blockedlst, &sem.cond.blocked));
+    tassert(is_process_in(&p2->sched.blockedlst, &sem.cond.blocked));
+
+    sem_release(&sem);
+    sleep(1);
+    tassert(p2->sched.status == PROC_STATUS_ZOMBIE);
+    tassert(p1->sched.status != PROC_STATUS_ZOMBIE);
+    tassert(p0->sched.status != PROC_STATUS_ZOMBIE);
+
+    sem_release(&sem);
+    sleep(1);
+    tassert(p1->sched.status == PROC_STATUS_ZOMBIE);
+    tassert(p0->sched.status != PROC_STATUS_ZOMBIE);
+
+    sem_release(&sem);
+    sleep(1);
+    tassert(p0->sched.status == PROC_STATUS_ZOMBIE);
 TEND
