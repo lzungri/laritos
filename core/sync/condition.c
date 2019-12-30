d9e418dab4f1881f02a086c7a843bb25c1e1035f
Leandro Zungri <lzungri@gmail.com>
2019-12-29 17:02:10 -0800

    condition: Add notify function to wake up all blocked processes

--- core/sync/condition.c
+++ core/sync/condition.c
@@ -1,6 +1,6 @@
-#define DEBUG
 #include <log.h>
 
+#include <stdbool.h>
 #include <core.h>
 #include <process/core.h>
 #include <utils/assert.h>
@@ -36,16 +36,33 @@ void condition_wait_locked(condition_t *cond, spinlock_t *spin, irqctx_t *ctx) {
     spinlock_acquire(spin, ctx);
 }
 
+static inline void wakeup_pcb(pcb_t *pcb, condition_t *cond) {
+    if (pcb == NULL) {
+        return;
+    }
+
+    list_del_init(&pcb->sched.blockedlst);
+
+    verbose_async("Waking up pid=%u waiting for condition=0x%p", pcb->pid, cond);
+    irqctx_t ctx2;
+    spinlock_acquire(&_laritos.proclock, &ctx2);
+    sched_move_to_ready_locked(pcb);
+    spinlock_release(&_laritos.proclock, &ctx2);
+}
+
 pcb_t *condition_notify_locked(condition_t *cond) {
     pcb_t *pcb = list_first_entry_or_null(&cond->blocked, pcb_t, sched.blockedlst);
-    if (pcb != NULL) {
-        list_del_init(&pcb->sched.blockedlst);
-
-        verbose_async("Waking up pid=%u waiting for condition=0x%p", pcb->pid, cond);
-        irqctx_t ctx2;
-        spinlock_acquire(&_laritos.proclock, &ctx2);
-        sched_move_to_ready_locked(pcb);
-        spinlock_release(&_laritos.proclock, &ctx2);
-    }
+    wakeup_pcb(pcb, cond);
     return pcb;
+}
+
+bool condition_notify_all_locked(condition_t *cond) {
+    pcb_t *pcb;
+    pcb_t *tmp;
+    bool proc_awakened = false;
+    list_for_each_entry_safe(pcb, tmp, &cond->blocked, sched.blockedlst) {
+        wakeup_pcb(pcb, cond);
+        proc_awakened = true;
+    }
+    return proc_awakened;
 }
--- include/sync/condition.h
+++ include/sync/condition.h
@@ -2,6 +2,7 @@
 
 #include <log.h>
 
+#include <stdbool.h>
 #include <process/core.h>
 #include <sync/spinlock.h>
 #include <dstruct/list.h>
@@ -13,10 +14,11 @@ typedef struct {
 int condition_init(condition_t *cond);
 void condition_wait_locked(condition_t *cond, spinlock_t *spin, irqctx_t *ctx);
 pcb_t *condition_notify_locked(condition_t *cond);
+bool condition_notify_all_locked(condition_t *cond);
 
 #define CONDITION_STATIC_INIT(_cond) { .blocked = LIST_HEAD_INIT(_cond.blocked), }
 
 #define SLEEP_UNTIL(_expr, _cond, _spin, _ctx) \
     while (!(_expr)) { \
-        condition_wait_spinlock(_cond, _spin, _ctx); \
+        condition_wait_locked(_cond, _spin, _ctx); \
     }
