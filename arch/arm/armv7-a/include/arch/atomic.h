3a656b5568065e98d18000cb413637ed0a64ea33
Leandro Zungri <lzungri@gmail.com>
2020-01-06 00:52:20 -0800

    arm: Implemented atomic64_t functionality for arm

--- arch/arm/armv7-a/include/arch/atomic.h
+++ arch/arm/armv7-a/include/arch/atomic.h
@@ -4,17 +4,7 @@
 #include <stdbool.h>
 
 /**
- * 32-bit atomic type for ARM.
- * Must be 4-byte aligned to ensure read/write atomicity.
- */
-typedef int32_t atomic32_t __attribute__ ((aligned (4)));
-
-
-#define ATOMIC32_INIT(_value) { _value }
-
-
-/**
- * According to ARM ARM document:
+ * From the ARM ARM document:
  *    In ARMv7, the single-copy atomic processor accesses are:
  *    • All byte accesses.
  *    • All halfword accesses to halfword-aligned locations.
@@ -29,6 +19,27 @@ typedef int32_t atomic32_t __attribute__ ((aligned (4)));
  *      which the STREXD succeeds, and use the read values.
  */
 
+
+/**
+ * 32-bit atomic type for ARM.
+ * Must be 4-byte aligned to ensure read/write atomicity.
+ */
+typedef int32_t atomic32_t __attribute__ ((aligned (4)));
+
+/**
+ * 64-bit atomic type for ARM.
+ * Must be 8-byte aligned to ensure read/write atomicity.
+ *
+ * TODO: For some reason aligned(8) doesn't seem to be working so I worked around
+ * it with aligned(16)
+ */
+typedef int64_t atomic64_t __attribute__ ((aligned (16)));
+
+
+#define ATOMIC32_INIT(_value) { _value }
+#define ATOMIC64_INIT(_value) { _value }
+
+
 static inline void arch_atomic32_init(atomic32_t *at, int32_t value) {
     *at = value;
 }
@@ -37,14 +48,14 @@ static inline int32_t arch_atomic32_get(atomic32_t *at) {
     return *at;
 }
 
-static inline int32_t arch_atomic32_inc(atomic32_t *at) {
+static inline int32_t arch_atomic32_add(atomic32_t *at, int32_t v) {
     int32_t prev = 0;
     bool strex_failed = true;
     asm volatile (
         /* Load *at into prev and notify the exclusive monitor */
         "1: ldrex %0, [%2]     \n"
-        /* Increment prev */
-        "   add %0, %0, #1     \n"
+        /* Add v to prev */
+        "   add %0, %0, %3     \n"
         /* Store prev into *at */
         "   strex %1, %0, [%2] \n"
         /* Check whether the store operation was done in an exclusive manner */
@@ -53,26 +64,41 @@ static inline int32_t arch_atomic32_inc(atomic32_t *at) {
         "   beq 1b             \n"
         : "=&r" (prev) /* Declare as output, we want this var to be updated
                           with the last value written to its associated register*/
-        : "r" (strex_failed), "r" (at)
+        : "r" (strex_failed), "r" (at), "r" (v)
         : "memory" /* Memory barrier (do not reorder read/writes)*/,
           "cc" /* Tell gcc this code modifies the cpsr */);
     return prev;
+}
+
+static inline int32_t arch_atomic32_inc(atomic32_t *at) {
+    return arch_atomic32_add(at, 1);
 }
 
 static inline int32_t arch_atomic32_dec(atomic32_t *at) {
-    int32_t prev = 0;
+    return arch_atomic32_add(at, -1);
+}
+
+static inline int64_t arch_atomic32_set(atomic32_t *at, int64_t value) {
+    return *at = value;
+}
+
+
+static inline void arch_atomic64_init(atomic64_t *at, int64_t value) {
+    *at = value;
+}
+
+static inline int64_t arch_atomic64_get(atomic64_t *at) {
+    int64_t prev = 0;
     bool strex_failed = true;
     asm volatile (
         /* Load *at into prev and notify the exclusive monitor */
-        "1: ldrex %0, [%2]     \n"
-        /* Decrement prev */
-        "   sub %0, %0, #1     \n"
+        "1: ldrexd %0, [%2]     \n"
         /* Store prev into *at */
-        "   strex %1, %0, [%2] \n"
+        "   strexd %1, %0, [%2] \n"
         /* Check whether the store operation was done in an exclusive manner */
-        "   cmp %1, #1         \n"
+        "   cmp %1, #1          \n"
         /* If not (=1), then keep trying */
-        "   beq 1b             \n"
+        "   beq 1b              \n"
         : "=&r" (prev) /* Declare as output, we want this var to be updated
                           with the last value written to its associated register*/
         : "r" (strex_failed), "r" (at)
@@ -81,6 +107,56 @@ static inline int32_t arch_atomic32_dec(atomic32_t *at) {
     return prev;
 }
 
-static inline int32_t arch_atomic32_set(atomic32_t *at, int32_t value) {
-    return *at = value;
+static inline int64_t arch_atomic64_add(atomic64_t *at, int64_t v) {
+    int64_t res = 0;
+    bool strex_failed = true;
+    asm volatile (
+        /* Load *at into res (%0 for the lowest register and %H0 for the highest reg).
+         * Also, notify the exclusive monitor */
+        "1: ldrexd %0, %H0, [%2]     \n"
+        /* Add (S = updating the conditions flags) least significant halves of v
+         * and res */
+        "   adds %Q0, %Q0, %Q3       \n"
+        /* Add (C = add the carry of the previous operation) the most significant
+         * halves of v and res */
+        "   adc %R0, %R0, %R3        \n"
+        /* Store lowest and highest register associated with res into *at */
+        "   strexd %1, %0, %H0, [%2] \n"
+        /* Check whether the store operation was done in an exclusive manner */
+        "   cmp %1, #1               \n"
+        /* If not (=1), then keep trying */
+        "   beq 1b                   \n"
+        : "=&r" (res)
+        : "r" (strex_failed), "r" (at), "r" (v)
+        : "memory" /* Memory barrier (do not reorder read/writes)*/,
+          "cc" /* Tell gcc this code modifies the cpsr */);
+    return res;
+}
+
+static inline int64_t arch_atomic64_inc(atomic64_t *at) {
+    return arch_atomic64_add(at, 1);
+}
+
+static inline int64_t arch_atomic64_dec(atomic64_t *at) {
+    return arch_atomic64_add(at, -1);
+}
+
+static inline int64_t arch_atomic64_set(atomic64_t *at, int64_t value) {
+    int64_t prev = 0;
+    bool strex_failed = true;
+    asm volatile (
+        /* Load *at into prev and notify the exclusive monitor */
+        "1: ldrexd %0, [%2]     \n"
+        /* Store value into *at */
+        "   strexd %1, %3, [%2] \n"
+        /* Check whether the store operation was done in an exclusive manner */
+        "   cmp %1, #1          \n"
+        /* If not (=1), then keep trying */
+        "   beq 1b              \n"
+        : "=&r" (prev) /* Declare as output, we want this var to be updated
+                          with the last value written to its associated register*/
+        : "r" (strex_failed), "r" (at), "r" (value)
+        : "memory" /* Memory barrier (do not reorder read/writes)*/,
+          "cc" /* Tell gcc this code modifies the cpsr */);
+    return value;
 }
