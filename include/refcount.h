0a5614d45517d8c2aeb6e8b5b1067b7afaf77f4b
Leandro Zungri <lzungri@gmail.com>
2020-01-06 14:53:16 -0800

    refcount: Use atomic32_t as reference counter

--- include/refcount.h
+++ include/refcount.h
@@ -3,34 +3,37 @@
 #include <log.h>
 #include <stdint.h>
 #include <limits.h>
+#include <sync/atomic.h>
 #include <utils/assert.h>
 
 typedef struct refcount {
-    uint32_t value;
+    atomic32_t value;
     void (*free)(struct refcount *ref);
 } refcount_t;
 
 static inline void ref_init(refcount_t *ref, void (*freecb)(refcount_t *ref)) {
-    ref->value = 0;
+    atomic32_init(&ref->value, 0);
     ref->free = freecb;
 }
 
 static inline uint32_t ref_get(refcount_t *ref) {
-    return ref->value;
+    return atomic32_get(&ref->value);
 }
 
 static inline uint32_t ref_inc(refcount_t *ref) {
     verbose_async("ref=0x%p inc", ref);
-    assert(ref->value < U32_MAX, "Reference counter at 0x%p cannot be bigger than %lu", ref, U32_MAX);
-    return ref->value++;
+    int32_t v = atomic32_inc(&ref->value);
+    assert(v > 0, "Invalid value for reference counter at 0x%p, value=%ld", ref, v);
+    return v;
 }
 
 static inline uint32_t ref_dec(refcount_t *ref) {
     verbose_async("ref=0x%p dec", ref);
-    assert(ref->value > 0, "Reference counter at 0x%p cannot be negative", ref);
-    if (--ref->value == 0 && ref->free != NULL) {
+    int32_t v = atomic32_dec(&ref->value);
+    assert(v >= 0, "Reference counter at 0x%p cannot be negative, value=%ld", ref, v);
+    if (v == 0 && ref->free != NULL) {
         verbose_async("refcount = 0, freeing objected referenced by ref=0x%p", ref);
         ref->free(ref);
     }
-    return ref->value;
+    return v;
 }
