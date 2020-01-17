29f4e1e472b50eff03fd8764d4a16a798ce44308
Leandro Zungri <lzungri@gmail.com>
2020-01-16 16:26:26 -0800

    latency: Add macro for benchmarking code

---
+++ include/utils/latency.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <log.h>
+#include <stdint.h>
+#include <limits.h>
+#include <time/time.h>
+#include <irq.h>
+
+#define LATENCY(_tag, _expr) do { \
+        static uint64_t _avg = 0; \
+        static uint32_t _count = 0; \
+        static uint64_t _min = U64_MAX; \
+        static uint64_t _max = 0; \
+        uint64_t _start = time_get_monotonic_cpu_cycles(); \
+            (_expr); \
+        uint64_t _diff = time_get_monotonic_cpu_cycles() - _start; \
+        _avg += _diff; \
+        _count++; \
+        if (_diff > _max) { \
+            _max = _diff; \
+        } \
+        if (_diff < _min) { \
+            _min = _diff; \
+        } \
+        info_async(_tag " #%-4lu | cur=%llu min=%llu max=%llu avg=%llu", _count, _diff, _min, _max, _avg / _count); \
+    } while (0);
+
+#define LATENCY_NOIRQ(_tag, _expr) do { \
+        irqctx_t ctx; \
+        irq_disable_local_and_save_ctx(&ctx); \
+        LATENCY(_tag, _expr); \
+        irq_local_restore_ctx(&ctx); \
+    } while (0);
