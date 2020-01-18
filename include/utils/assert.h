d3bda8a776f726234a31425de3db012ffa9377d0
Leandro Zungri <lzungri@gmail.com>
2020-01-17 17:44:11 -0800

    assert: Add file + line # into the assertion message

--- include/utils/assert.h
+++ include/utils/assert.h
@@ -3,5 +3,8 @@
 #include <log.h>
 
 #define assert(_cond, _msg, ...) do { \
-        if (!(_cond)) fatal(_msg, ##__VA_ARGS__); \
+        if (!(_cond)) { \
+            error("[ " #_cond " ] failed in " __FILE__ ":" TOSTRING(__LINE__)); \
+            fatal(_msg, ##__VA_ARGS__); \
+        } \
     } while(0)
