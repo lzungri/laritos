24af930cb4b53739b2651676ca8ab7f1a21f0b8b
Leandro Zungri <lzungri@gmail.com>
2020-01-19 18:53:56 -0800

    cpu: Add test for cycle count overflow handling

--- test/tests/core/cpu.c
+++ test/tests/core/cpu.c
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include <cpu.h>
+#include <limits.h>
 #include <test/test.h>
 #include <time/time.h>
 #include <utils/latency.h>
@@ -30,17 +31,17 @@ T(cpu_cycle_count_resets_when_disabled) {
     tassert(cpu_get_cycle_count() >= 0);
 TEND
 
-T(cpu_cycle_count_properly_handles_bigger_than_32bits_values) {
+T(cpu_cycle_count_properly_handles_values_bigger_than_32bits) {
     cpu_set_cycle_count_enable(true);
     uint64_t count;
     uint64_t countprev = 0;
     int i;
-    // Counter should reach a > 32bits value before the 30 seconds mark
-    for (i = 0; i < 30; i++) {
+    // Counter overflows every (U32_MAX / cpufreq) seconds, we wait that amount times 2
+    uint64_t cpufreq = 1000000000;
+    for (i = 0; i < (U32_MAX / cpufreq) * 2; i++) {
         sleep(1);
         count = cpu_get_cycle_count();
-        // Counter should always increase, if not, then there was probably an overflow
-        info("c=%llu", count);
+        // Counter should always increase, if not, there was probably an unhandled overflow
         tassert(countprev < count);
         countprev = count;
     }
