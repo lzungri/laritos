9a0e04e7d8f9b35caf00bba5a3355a893cb0d715
Leandro Zungri <lzungri@gmail.com>
2020-01-20 17:57:09 -0800

    latency: Add test cases for the LATENCY macro

--- test/tests/utils/latency.c
+++ test/tests/utils/latency.c
@@ -12,11 +12,38 @@ T(latency_latencynoirq_disable_irqs_as_expected) {
     });
 TEND
 
-T(latency_logs_latency_info) {
+T(latency_logs_latency_info_1us_sleep) {
     int i;
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < 100; i++) {
+        LATENCY("usleep(1)", {
+            usleep(1);
+        });
+    }
+TEND
+
+T(latency_logs_latency_info_1ms_sleep) {
+    int i;
+    for (i = 0; i < 100; i++) {
+        LATENCY("msleep(1)", {
+            msleep(1);
+        });
+    }
+TEND
+
+T(latency_logs_latency_info_1sec_sleep) {
+    int i;
+    for (i = 0; i < 10; i++) {
         LATENCY("sleep(1)", {
             sleep(1);
+        });
+    }
+TEND
+
+T(latency_logs_latency_info_5sec_sleep) {
+    int i;
+    for (i = 0; i < 3; i++) {
+        LATENCY("sleep(5)", {
+            sleep(5);
         });
     }
 TEND
