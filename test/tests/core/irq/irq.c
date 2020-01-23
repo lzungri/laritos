565f97a62ff98f0f2600e87b20e8c2d597d3bc2e
Leandro Zungri <lzungri@gmail.com>
2020-01-23 13:59:01 -0800

    irq: Add more test cases

--- test/tests/core/irq/irq.c
+++ test/tests/core/irq/irq.c
@@ -1,8 +1,10 @@
 #include <log.h>
 
+#include <stdbool.h>
 #include <stdint.h>
 #include <test/test.h>
 #include <irq/core.h>
+#include <irq/types.h>
 
 T(irq_disable_enable_works_as_expected) {
     irq_disable_local();
@@ -11,20 +13,41 @@ T(irq_disable_enable_works_as_expected) {
     tassert(irq_is_enabled());
 TEND
 
-T(irq_enable_disable_calls_should_match_to_actually_change_irqs) {
-    irq_disable_local();
-    tassert(!irq_is_enabled());
-    irq_disable_local();
-    tassert(!irq_is_enabled());
-
-    irq_enable_local();
-    tassert(!irq_is_enabled());
+T(irq_enable_disable_calls_work_as_expected) {
+    irqctx_t ctx;
+    irq_save_context(&ctx);
 
     irq_disable_local();
     tassert(!irq_is_enabled());
-
-    irq_enable_local();
+    irq_disable_local();
     tassert(!irq_is_enabled());
+
     irq_enable_local();
     tassert(irq_is_enabled());
+
+    irq_restore_context(&ctx);
+TEND
+
+T(irq_enable_disable_with_ctx_calls_work_as_expected) {
+    bool irq_orig_enabled = irq_is_enabled();
+
+    irqctx_t ctx;
+    irq_disable_local_and_save_ctx(&ctx);
+    {
+        tassert(!irq_is_enabled());
+        tassert(irq_is_enabled_in_ctx(&ctx) == irq_orig_enabled);
+
+        irqctx_t ctx2;
+        irq_disable_local_and_save_ctx(&ctx2);
+        {
+            tassert(!irq_is_enabled());
+            tassert(!irq_is_enabled_in_ctx(&ctx2));
+        }
+        arch_irq_restore_context(&ctx2);
+
+        tassert(!irq_is_enabled());
+    }
+    arch_irq_restore_context(&ctx);
+
+    tassert(irq_is_enabled() == irq_orig_enabled);
 TEND
