f4b551a0475a540007e475a372048ea1bd5810fb
Leandro Zungri <lzungri@gmail.com>
2020-01-25 00:23:37 -0800

    cond: Always require a spinlock for ensuring thread safety on SMP

--- core/sync/condition.c
+++ core/sync/condition.c
@@ -13,85 +13,38 @@ int condition_init(condition_t *cond) {
     return 0;
 }
 
-static void condition_block_current_process(condition_t *cond, bool irq_disabled) {
-    pcb_t *pcb = process_get_current();
-
-    if (irq_disabled) {
-        sched_move_to_blocked_locked(pcb, &cond->blocked);
-    } else {
-        irqctx_t ctx2;
-        irq_disable_local_and_save_ctx(&ctx2);
-        sched_move_to_blocked_locked(pcb, &cond->blocked);
-        irq_local_restore_ctx(&ctx2);
-    }
-
-    verbose_async("pid=%u waiting for condition=0x%p", pcb->pid, cond);
-}
-
 void condition_wait_locked(condition_t *cond, spinlock_t *spin, irqctx_t *ctx) {
-    condition_block_current_process(cond, false);
+    pcb_t *pcb = process_get_current();
+    sched_move_to_blocked_locked(pcb, &cond->blocked);
+    verbose_async("pid=%u waiting for condition=0x%p", pcb->pid, cond);
 
     spinlock_release(spin, ctx);
     schedule();
     spinlock_acquire(spin, ctx);
 }
 
-void condition_wait_irq_disabled(condition_t *cond, irqctx_t *ctx) {
-    condition_block_current_process(cond, true);
-
-    irq_local_restore_ctx(ctx);
-    schedule();
-    irq_disable_local_and_save_ctx(ctx);
-}
-
-static inline void wakeup_pcb(pcb_t *pcb, condition_t *cond, bool irq_disabled) {
+static inline void wakeup_pcb_locked(pcb_t *pcb, condition_t *cond) {
     if (pcb == NULL) {
         return;
     }
-
-    list_del_init(&pcb->sched.sched_node);
-
     verbose_async("Waking up pid=%u waiting for condition=0x%p", pcb->pid, cond);
-
-    if (irq_disabled) {
-        sched_move_to_ready_locked(pcb);
-    } else {
-        irqctx_t ctx;
-        irq_disable_local_and_save_ctx(&ctx);
-        sched_move_to_ready_locked(pcb);
-        irq_local_restore_ctx(&ctx);
-    }
-}
-
-static inline pcb_t *notify_locked(condition_t *cond, bool irq_disabled) {
-    pcb_t *pcb = list_first_entry_or_null(&cond->blocked, pcb_t, sched.sched_node);
-    wakeup_pcb(pcb, cond, irq_disabled);
-    return pcb;
-}
-
-pcb_t *condition_notify_irq_disabled(condition_t *cond) {
-    return notify_locked(cond, true);
+    list_del_init(&pcb->sched.sched_node);
+    sched_move_to_ready_locked(pcb);
 }
 
 pcb_t *condition_notify_locked(condition_t *cond) {
-    return notify_locked(cond, false);
+    pcb_t *pcb = list_first_entry_or_null(&cond->blocked, pcb_t, sched.sched_node);
+    wakeup_pcb_locked(pcb, cond);
+    return pcb;
 }
 
-bool notify_all_locked(condition_t *cond, bool irq_disabled) {
+bool condition_notify_all_locked(condition_t *cond) {
     pcb_t *pcb;
     pcb_t *tmp;
     bool proc_awakened = false;
     list_for_each_entry_safe(pcb, tmp, &cond->blocked, sched.sched_node) {
-        wakeup_pcb(pcb, cond, irq_disabled);
+        wakeup_pcb_locked(pcb, cond);
         proc_awakened = true;
     }
     return proc_awakened;
-}
-
-bool condition_notify_all_irq_disabled(condition_t *cond) {
-    return notify_all_locked(cond, true);
-}
-
-bool condition_notify_all_locked(condition_t *cond) {
-    return notify_all_locked(cond, false);
 }
