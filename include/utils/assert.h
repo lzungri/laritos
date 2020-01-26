9b4b34de5dd933ae75413ae171013843336d208c
Leandro Zungri <lzungri@gmail.com>
2020-01-26 12:38:25 -0800

    assert: Add option not to print any log (in case log mechanism is corrupted)

--- include/utils/assert.h
+++ include/utils/assert.h
@@ -1,10 +1,19 @@
 #pragma once
 
 #include <log.h>
+#include <generated/autoconf.h>
 
+#ifdef CONFIG_ASSERT_PRINT_MSG
 #define assert(_cond, _msg, ...) do { \
         if (!(_cond)) { \
             error("[ " #_cond " ] failed in " __FILE__ ":" TOSTRING(__LINE__)); \
             fatal(_msg, ##__VA_ARGS__); \
         } \
     } while(0)
+#else
+#define assert(_cond, _msg, ...) do { \
+        if (!(_cond)) { \
+            while (1); \
+        } \
+    } while(0)
+#endif
