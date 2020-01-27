5f471d67c520d87254388b8dbb8daa5a0f9b2c19
Leandro Zungri <lzungri@gmail.com>
2020-01-26 23:43:46 -0800

    random: Add documentation about the LCG

--- core/utils/random-lcg.c
+++ core/utils/random-lcg.c
@@ -12,6 +12,13 @@ void random_seed(uint32_t seed) {
  * Same LCG (Linear Congruential Generator) used by glicb, basic and exploitable.
  */
 int32_t random_int32(int32_t min, int32_t max) {
+    /**
+     * X(n+1) = (X(n) * a + c) % m
+     *
+     * multiplier a = 1103515245
+     * increment c  = 12345
+     * modulus m    = 2^31
+     */
     _laritos.rndseed = (_laritos.rndseed * 1103515245 + 12345) & 0x7fffffff;
     return _laritos.rndseed % (max - min + 1) + min;
 }
