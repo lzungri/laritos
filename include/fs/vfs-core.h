6125aef6b95d55a97bb1b99ed589e8a3cdbc6e5a
Leandro Zungri <lzungri@gmail.com>
2020-01-30 17:51:53 -0800

    vfs: Add macro to declare FS type modules

--- include/fs/vfs-core.h
+++ include/fs/vfs-core.h
@@ -3,8 +3,24 @@
 #include <stdint.h>
 #include <fs/vfs-types.h>
 
-int vfs_init(void);
 int vfs_register_fs_type(fs_type_t *fst);
 int vfs_unregister_fs_type(fs_type_t *fst);
 fs_mount_t *vfs_mount_fs(char *fstype, char *mount_point, uint16_t flags, void *params);
 int vfs_unmount_fs(char *mount_point);
+
+
+#define FS_TYPE_MODULE(_id, _mount) \
+    static fs_type_t _fstype_ ## _id = { \
+        .id = #_id, \
+        .mount = (_mount), \
+    }; \
+    \
+    static int _init_ ## _id(module_t *m) { \
+        return vfs_register_fs_type(&_fstype_ ## _id); \
+    } \
+    \
+    static int _deinit_ ## _id(module_t *m) { \
+        return vfs_unregister_fs_type(&_fstype_ ## _id); \
+    } \
+    \
+    MODULE(_id, _init_ ## _id, _deinit_ ## _id)
