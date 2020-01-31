6ce5bbc231985aace7ebd5b97eceb10a709545d3
Leandro Zungri <lzungri@gmail.com>
2020-01-31 14:37:29 -0800

    vfs: Add test cases

--- test/tests/core/fs/vfs-core.c
+++ test/tests/core/fs/vfs-core.c
@@ -6,14 +6,6 @@
 #include <fs/vfs-core.h>
 #include <fs/vfs-types.h>
 
-static bool is_fstype_registered(fs_type_t *fst) {
-    return false;
-}
-
-static bool is_fs_mounted(char *mount_point) {
-    return false;
-}
-
 static fs_mount_t *empty_mount(fs_type_t *fstype, char *mount_point, uint16_t flags, void *params) {
     return NULL;
 }
@@ -23,11 +15,11 @@ T(vfs_register_fs_type_adds_a_new_fstype_to_the_supported_systems) {
         .id = "newfs",
         .mount = empty_mount,
     };
-    tassert(vfs_register_fs_type(&fst));
-    tassert(is_fstype_registered(&fst));
+    vfs_register_fs_type(&fst);
+    tassert(vfs_is_fs_type_supported(fst.id));
 
     vfs_unregister_fs_type(&fst);
-    tassert(!is_fstype_registered(&fst));
+    tassert(!vfs_is_fs_type_supported(fst.id));
 TEND
 
 T(vfs_unregister_fstype_fails_on_nonregistered_fstype) {
@@ -58,8 +50,8 @@ T(vfs_cannot_register_two_or_more_fstypes_with_the_same_id) {
         .id = "testfs",
         .mount = empty_mount,
     };
-    tassert(vfs_register_fs_type(&fst));
-    tassert(is_fstype_registered(&fst));
+    vfs_register_fs_type(&fst);
+    tassert(vfs_is_fs_type_supported(fst.id));
 
     fs_type_t fst2 = {
         .id = "testfs",
@@ -68,11 +60,12 @@ T(vfs_cannot_register_two_or_more_fstypes_with_the_same_id) {
     tassert(vfs_register_fs_type(&fst2) < 0);
 
     vfs_unregister_fs_type(&fst);
-    tassert(!is_fstype_registered(&fst));
+    tassert(!vfs_is_fs_type_supported(fst.id));
 TEND
 
 T(vfs_mount_fails_on_unsupported_fs_type) {
-    tassert(vfs_mount_fs("unsopported", "xxx", 0, NULL) == NULL);
+    tassert(vfs_mount_fs("unsopported", "/xxx", 0, NULL) == NULL);
+    tassert(!vfs_is_fs_mounted("/xxx"));
 TEND
 
 T(vfs_mount_fails_on_mount_error) {
@@ -80,13 +73,14 @@ T(vfs_mount_fails_on_mount_error) {
         .id = "testfs",
         .mount = empty_mount,
     };
-    tassert(vfs_register_fs_type(&fst));
-    tassert(is_fstype_registered(&fst));
+    vfs_register_fs_type(&fst);
+    tassert(vfs_is_fs_type_supported(fst.id));
 
-    tassert(vfs_mount_fs("testfs", "xxx", 0, NULL) == NULL);
+    tassert(vfs_mount_fs("testfs", "/xxx", 0, NULL) == NULL);
+    tassert(!vfs_is_fs_mounted("/xxx"));
 
     vfs_unregister_fs_type(&fst);
-    tassert(!is_fstype_registered(&fst));
+    tassert(!vfs_is_fs_type_supported(fst.id));
 TEND
 
 T(vfs_unmount_fs_fails_on_non_mounted_fs) {
@@ -104,19 +98,70 @@ static fs_mount_t *dummy_mount(fs_type_t *fstype, char *mount_point, uint16_t fl
 
 T(vfs_mount_adds_a_new_fs_under_mount_point) {
     fs_type_t fst = {
-        .id = "testfs",
+        .id = "dummymnt",
         .mount = dummy_mount,
     };
-    tassert(vfs_register_fs_type(&fst));
-    tassert(is_fstype_registered(&fst));
+    vfs_register_fs_type(&fst);
+    tassert(vfs_is_fs_type_supported(fst.id));
 
-    fs_mount_t *fsm = vfs_mount_fs("testfs", "/test", FS_MOUNT_READ | FS_MOUNT_WRITE, NULL);
+    fs_mount_t *fsm = vfs_mount_fs("dummymnt", "/dummymnt", FS_MOUNT_READ | FS_MOUNT_WRITE, NULL);
     tassert(fsm == &dummy_mnt);
     tassert(fsm->flags == (FS_MOUNT_READ | FS_MOUNT_WRITE));
-    tassert(strncmp(fsm->mount_point, "/test", sizeof(fsm->mount_point)) == 0);
+    tassert(strncmp(fsm->mount_point, "/dummymnt", sizeof(fsm->mount_point)) == 0);
     tassert(fsm->sb->fstype == &fst);
-    tassert(is_fs_mounted("/test"));
+    tassert(vfs_is_fs_mounted("/dummymnt"));
 
-    vfs_unmount_fs("/test");
-    tassert(!is_fs_mounted("/test"));
+    vfs_unmount_fs("/dummymnt");
+    tassert(!vfs_is_fs_mounted("/dummymnt"));
+
+    vfs_unregister_fs_type(&fst);
+    tassert(!vfs_is_fs_type_supported(fst.id));
+TEND
+
+fs_mount_t nosb_mnt = {
+    .sb = NULL,
+};
+
+static fs_mount_t *nosb_mount(fs_type_t *fstype, char *mount_point, uint16_t flags, void *params) {
+    return &nosb_mnt;
+}
+
+T(vfs_mount_fails_when_no_superblock_is_instantiated) {
+    fs_type_t fst = {
+        .id = "testnosb",
+        .mount = nosb_mount,
+    };
+    vfs_register_fs_type(&fst);
+    tassert(vfs_is_fs_type_supported(fst.id));
+
+    tassert(vfs_mount_fs("testnosb", "/testnosb", FS_MOUNT_READ | FS_MOUNT_WRITE, NULL) == NULL);
+    tassert(!vfs_is_fs_mounted("/testnosb"));
+
+    vfs_unregister_fs_type(&fst);
+    tassert(!vfs_is_fs_type_supported(fst.id));
+TEND
+
+T(vfs_mount_fails_if_mount_point_is_already_used) {
+    fs_type_t fst = {
+        .id = "mnt_used",
+        .mount = dummy_mount,
+    };
+    vfs_register_fs_type(&fst);
+    tassert(vfs_is_fs_type_supported(fst.id));
+
+    fs_mount_t *fsm = vfs_mount_fs("mnt_used", "/mnt_used", FS_MOUNT_READ | FS_MOUNT_WRITE, NULL);
+    tassert(fsm != NULL);
+    tassert(vfs_is_fs_mounted("/mnt_used"));
+    tassert(vfs_mount_fs("mnt_used", "/mnt_used", FS_MOUNT_READ | FS_MOUNT_WRITE, NULL) == NULL);
+    tassert(vfs_is_fs_mounted("/mnt_used"));
+
+    vfs_unmount_fs("/mnt_used");
+    tassert(!vfs_is_fs_mounted("/mnt_used"));
+
+    vfs_unregister_fs_type(&fst);
+    tassert(!vfs_is_fs_type_supported(fst.id));
+TEND
+
+T(vfs_cannot_unmount_inexistent_mount_point) {
+    tassert(vfs_unmount_fs("/non_existent_mount_point") < 0);
 TEND
