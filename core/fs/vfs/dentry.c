b4b3fc764c7b94eeaa22ee86187e736c0152fe72
Leandro Zungri <lzungri@gmail.com>
2020-02-03 15:16:42 -0800

    dentry: Implement dentry lookup functionality

--- core/fs/vfs/dentry.c
+++ core/fs/vfs/dentry.c
@@ -7,6 +7,8 @@
 #include <dstruct/list.h>
 #include <fs/vfs/types.h>
 #include <fs/vfs/core.h>
+#include <mm/heap.h>
+#include <generated/autoconf.h>
 
 
 int vfs_dentry_init_root() {
@@ -14,6 +16,91 @@ int vfs_dentry_init_root() {
     INIT_LIST_HEAD(&_laritos.fs.root.siblings);
     strncpy(_laritos.fs.root.name, "/", sizeof(_laritos.fs.root.name));
     return 0;
+}
+
+void vfs_dentry_add_child(fs_dentry_t *parent, fs_dentry_t *child) {
+    list_add_tail(&child->siblings, &parent->children);
+    child->parent = parent;
+}
+
+void vfs_dentry_remove_as_child(fs_dentry_t *child) {
+    child->parent = NULL;
+    list_del_init(&child->siblings);
+}
+
+fs_dentry_t *vfs_dentry_alloc(char *name, fs_inode_t *inode, fs_dentry_t *parent) {
+    fs_dentry_t *d = calloc(1, sizeof(fs_dentry_t));
+    if (d == NULL) {
+        return NULL;
+    }
+    INIT_LIST_HEAD(&d->children);
+    INIT_LIST_HEAD(&d->siblings);
+    strncpy(d->name, name, sizeof(d->name));
+    d->inode = inode;
+
+    if (parent != NULL) {
+        vfs_dentry_add_child(parent, d);
+    }
+    return d;
+}
+
+void vfs_dentry_free(fs_dentry_t *d) {
+    free(d);
+}
+
+static inline fs_dentry_t *find_children(fs_dentry_t *parent, char *relpath) {
+    size_t namelen = strlen(relpath);
+    char *nextpath = strchr(relpath, '/');
+    if (nextpath != NULL) {
+        namelen = min(namelen, (size_t) (nextpath - relpath));
+    }
+
+    fs_dentry_t *d;
+    list_for_each_entry(d, &parent->children, siblings) {
+        if (strncmp(d->name, relpath, namelen) == 0 && d->name[namelen] == '\0') {
+            return d;
+        }
+    }
+    return NULL;
+}
+
+fs_dentry_t *vfs_dentry_lookup_from(fs_dentry_t *parent, char *relpath) {
+    if (relpath == NULL || strlen(relpath) == 0) {
+        return NULL;
+    }
+
+    fs_dentry_t *d = parent;
+    while (true) {
+        d = find_children(d, relpath);
+        if (d == NULL) {
+            break;
+        }
+        relpath = strchr(relpath, '/');
+        // If there are no more children to lookup or there is is a / with no
+        // children, e.g. a/b/c/
+        if (relpath == NULL || relpath[1] == '\0') {
+            break;
+        }
+        relpath++;
+    }
+    return d;
+}
+
+fs_dentry_t *vfs_dentry_lookup(char *path) {
+    if (path == NULL) {
+        return NULL;
+    }
+
+    if (path[0] == '/') {
+        if (path[1] == '\0') {
+            return &_laritos.fs.root;
+        }
+        path++;
+        return vfs_dentry_lookup_from(&_laritos.fs.root, path);
+    }
+
+    // TODO Use cwd dentry instead
+    return vfs_dentry_lookup_from(&_laritos.fs.root, path);
 }
 
 
--- include/fs/vfs/core.h
+++ include/fs/vfs/core.h
@@ -5,15 +5,24 @@
 #include <fs/vfs/types.h>
 
 int vfs_init_global_context(void);
-int vfs_dentry_init_root(void);
+
 fs_type_t *vfs_get_fstype(char *fstype);
 int vfs_register_fs_type(fs_type_t *fst);
 int vfs_unregister_fs_type(fs_type_t *fst);
+
 bool vfs_is_fs_mounted(char *mount_point);
 bool vfs_is_fs_type_supported(char *fstype);
 void vfs_initialize_mount_struct(fs_mount_t *mount, fs_type_t *fstype, char *mount_point, uint16_t flags, void *params);
 fs_mount_t *vfs_mount_fs(char *fstype, char *mount_point, uint16_t flags, void *params);
 int vfs_unmount_fs(char *mount_point);
+
+int vfs_dentry_init_root(void);
+fs_dentry_t *vfs_dentry_alloc(char *name, fs_inode_t *inode, fs_dentry_t *parent);
+void vfs_dentry_free(fs_dentry_t *d);
+void vfs_dentry_add_child(fs_dentry_t *parent, fs_dentry_t *child);
+void vfs_dentry_remove_as_child(fs_dentry_t *child);
+fs_dentry_t *vfs_dentry_lookup_from(fs_dentry_t *parent, char *relpath);
+fs_dentry_t *vfs_dentry_lookup(char *path);
 
 
 #define FILESYSTEM_MODULE(_id, _mount) \
