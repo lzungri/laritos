8ecf50dddcf93508103f5e12c5569b7f7d9fb274
Leandro Zungri <lzungri@gmail.com>
2020-02-06 15:30:31 -0800

    vfs: Add file-related source code

---
+++ core/fs/vfs/file.c
@@ -0,0 +1,11 @@
+#define DEBUG
+#include <log.h>
+
+#include <fs/vfs/types.h>
+#include <fs/vfs/core.h>
+#include <module/core.h>
+#include <mm/heap.h>
+
+fs_file_t *vfs_file_open(char *path, fs_access_mode_t mode) {
+    return NULL;
+}
