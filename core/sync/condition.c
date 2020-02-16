29cc5211038cc16f5ffbfa5c91432946d1155280
Leandro Zungri <lzungri@gmail.com>
2020-02-16 01:29:15 -0800

    condition: Hack! Use pcbs_data_lock as a reentrant lock

--- core/sync/condition.c
+++ core/sync/condition.c
@@ -13,13 +13,39 @@ int condition_init(condition_t *cond) {
     return 0;
 }
 
+/**
+ * HACK Alert: A condition can be used in different scenarios, some of them may or may not
+ * have the _laritos.proc.pcbs_data_lock lock held. In order to prevent deadlocks by
+ * re-acquiring the same lock, we use the following hack to only grab the spinlock if it
+ * wasn't already owned by the process.
+ */
+static inline bool grab_pcbsdatalock_if_not_held(irqctx_t *pcbdata_ctx) {
+    if (spinlock_trylock(&_laritos.proc.pcbs_data_lock, pcbdata_ctx)) {
+        return true;
+    } else if (!spinlock_owned_by_me(&_laritos.proc.pcbs_data_lock)) {
+        spinlock_acquire(&_laritos.proc.pcbs_data_lock, pcbdata_ctx);
+        return true;
+    }
+    return false;
+}
+
 void condition_wait_locked(condition_t *cond, spinlock_t *spin, irqctx_t *ctx) {
     pcb_t *pcb = process_get_current();
+
+    irqctx_t pcbdata_ctx;
+    bool pcbs_lock_acquired = grab_pcbsdatalock_if_not_held(&pcbdata_ctx);
+
     sched_move_to_blocked_locked(pcb, &cond->blocked);
+
+    if (pcbs_lock_acquired) {
+        spinlock_release(&_laritos.proc.pcbs_data_lock, &pcbdata_ctx);
+    }
+    spinlock_release(spin, ctx);
+
     verbose_async("pid=%u waiting for condition=0x%p", pcb->pid, cond);
 
-    spinlock_release(spin, ctx);
     schedule();
+
     spinlock_acquire(spin, ctx);
 }
 
@@ -28,8 +54,16 @@ static inline void wakeup_pcb_locked(pcb_t *pcb, condition_t *cond) {
         return;
     }
     verbose_async("Waking up pid=%u waiting for condition=0x%p", pcb->pid, cond);
+
+    irqctx_t pcbdata_ctx;
+    bool pcbs_lock_acquired = grab_pcbsdatalock_if_not_held(&pcbdata_ctx);
+
     list_del_init(&pcb->sched.sched_node);
     sched_move_to_ready_locked(pcb);
+
+    if (pcbs_lock_acquired) {
+        spinlock_release(&_laritos.proc.pcbs_data_lock, &pcbdata_ctx);
+    }
 }
 
 pcb_t *condition_notify_locked(condition_t *cond) {
--- include/sync/condition.h
+++ include/sync/condition.h
@@ -12,15 +12,17 @@ typedef struct {
 
 int condition_init(condition_t *cond);
 /**
- * Note: Must be called with _laritos.proc.pcbs_data_lock held
+ * Note: Must be called with <spin> lock held
  */
 void condition_wait_locked(condition_t *cond, spinlock_t *spin, irqctx_t *ctx);
 /**
- * Note: Must be called with _laritos.proc.pcbs_data_lock held
+ * Note: Must be called with <spin> lock held, where <spin> is the lock used in
+ * condition_wait_locked()
  */
 struct pcb *condition_notify_locked(condition_t *cond);
 /**
- * Note: Must be called with _laritos.proc.pcbs_data_lock held
+ * Note: Must be called with <spin> lock held, where <spin> is the lock used in
+ * condition_wait_locked()
  */
 bool condition_notify_all_locked(condition_t *cond);
 
@@ -28,7 +30,7 @@ bool condition_notify_all_locked(condition_t *cond);
 #define CONDITION_STATIC_INIT(_cond) { .blocked = LIST_HEAD_INIT(_cond.blocked), }
 
 /**
- * Note: Must be called with _laritos.proc.pcbs_data_lock held
+ * Note: Must be called with <spin> lock held
  */
 #define BLOCK_UNTIL(_expr, _cond, _spin, _ctx) \
     while (!(_expr)) { \
