24d8b36b9641f9f1ad70450971ef9259b4737ae9
Leandro Zungri <lzungri@gmail.com>
2020-02-19 14:14:36 -0800

    log: Add ability to log raw strings (i.e. no log metadata)

--- core/syscall/puts.c
+++ core/syscall/puts.c
@@ -4,6 +4,7 @@
 #include <syscall/syscall.h>
 
 int syscall_puts(const char *s) {
-    info("%s", s);
+    // Output process message as a raw string (i.e. no date, pid, tag metadata, etc)
+    __add_log_msg(true, NULL, NULL, (char *) s);
     return 0;
 }
