577d96111fa5496e5aa8bd96bd527a1ce8047b0f
Leandro Zungri <lzungri@gmail.com>
2020-02-18 16:16:39 -0800

    handler: Add function to dump info on generic errors

--- include/mm/exc-handlers.h
+++ include/mm/exc-handlers.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <process/types.h>
 #include <arch/context-types.h>
 
 void exc_dump_process_info(pcb_t *pcb);
@@ -9,6 +10,7 @@ void exc_dump_process_info(pcb_t *pcb);
  */
 void exc_dump_process_info_locked(pcb_t *pcb);
 void exc_handle_process_exception(pcb_t *pcb);
+void exc_handle_generic_process_error(pcb_t *pcb, spctx_t *ctx);
 void exc_undef_handler(int32_t pc, spctx_t *ctx);
 void exc_prefetch_handler(int32_t pc, spctx_t *ctx);
 void exc_abort_handler(int32_t pc, spctx_t *ctx);
