8a66c39f2faa82101026b902df142563e4736f62
Leandro Zungri <lzungri@gmail.com>
2020-02-24 00:53:59 -0800

    process: Add memory map and priority sysfs nodes

--- core/process/sysfs.c
+++ core/process/sysfs.c
@@ -20,6 +20,16 @@ static int name_read(fs_file_t *f, void *buf, size_t blen, uint32_t offset) {
     return ret;
 }
 
+static int prio_read(fs_file_t *f, void *buf, size_t blen, uint32_t offset) {
+    pcb_t *pcb = f->data0;
+    irqctx_t ctx;
+    spinlock_acquire(&_laritos.proc.pcbs_data_lock, &ctx);
+    char data[16];
+    int strlen = snprintf(data, sizeof(data), "%u", pcb->sched.priority);
+    spinlock_release(&_laritos.proc.pcbs_data_lock, &ctx);
+    return pseudofs_write_to_buf(buf, blen, data, strlen + 1, offset);
+}
+
 static int cwd_read(fs_file_t *f, void *buf, size_t blen, uint32_t offset) {
     pcb_t *pcb = f->data0;
     irqctx_t ctx;
@@ -89,6 +99,31 @@ static int syscalls_read(fs_file_t *f, void *buf, size_t blen, uint32_t offset) 
     return pseudofs_write_to_buf(buf, blen, data, totalb, offset);
 }
 
+static int maps_read(fs_file_t *f, void *buf, size_t blen, uint32_t offset) {
+    char data[512];
+    uint32_t totalb = 0;
+    int strlen;
+
+#define MAP(_sect, _start, _size) \
+    strlen = snprintf(data + totalb, sizeof(data) - totalb, "%-5.5s 0x%p-0x%p %lu%\n", \
+            #_sect, _start, (char *) _start + _size, _size); \
+    if (strlen < 0) { \
+        return -1; \
+    } \
+    totalb += strlen;
+
+    pcb_t *pcb = f->data0;
+    MAP(img, pcb->mm.imgaddr, pcb->mm.imgsize);
+    MAP(text, pcb->mm.text_start, pcb->mm.text_size);
+    MAP(data, pcb->mm.data_start, pcb->mm.data_size);
+    MAP(bss, pcb->mm.bss_start, pcb->mm.bss_size);
+    MAP(got, pcb->mm.got_start, pcb->mm.got_size);
+    MAP(heap, pcb->mm.heap_start, pcb->mm.heap_size);
+    MAP(stack, pcb->mm.stack_bottom, pcb->mm.stack_size);
+
+    return pseudofs_write_to_buf(buf, blen, data, totalb, offset);
+}
+
 int process_sysfs_create(pcb_t *pcb) {
     char buf[CONFIG_FS_MAX_FILENAME_LEN];
     snprintf(buf, sizeof(buf), "%u", pcb->pid);
@@ -101,6 +136,9 @@ int process_sysfs_create(pcb_t *pcb) {
 
     if (pseudofs_create_custom_ro_file_with_dataptr(dir, "name", name_read, pcb) == NULL) {
         error("Failed to create 'name' sysfs file for pid=%u", pcb->pid);
+    }
+    if (pseudofs_create_custom_ro_file_with_dataptr(dir, "prio", prio_read, pcb) == NULL) {
+        error("Failed to create 'maps' sysfs file for pid=%u", pcb->pid);
     }
     if (pseudofs_create_bool_file(dir, "kernel", FS_ACCESS_MODE_READ, &pcb->kernel) == NULL) {
         error("Failed to create 'kernel' sysfs file for pid=%u", pcb->pid);
@@ -126,6 +164,9 @@ int process_sysfs_create(pcb_t *pcb) {
     }
     if (pseudofs_create_custom_ro_file_with_dataptr(dir, "syscalls", syscalls_read, pcb) == NULL) {
         error("Failed to create 'syscalls' sysfs file for pid=%u", pcb->pid);
+    }
+    if (pseudofs_create_custom_ro_file_with_dataptr(dir, "maps", maps_read, pcb) == NULL) {
+        error("Failed to create 'maps' sysfs file for pid=%u", pcb->pid);
     }
 
     return 0;
