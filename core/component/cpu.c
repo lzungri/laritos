0a45f232ec44e3f7812b6d0fb434e3cbd5d4b004
Leandro Zungri <lzungri@gmail.com>
2020-02-25 19:44:05 -0800

    cpu: Implement sysfs nodes

--- core/component/cpu.c
+++ core/component/cpu.c
@@ -2,11 +2,52 @@
 
 #include <stdbool.h>
 #include <core.h>
+#include <printf.h>
 #include <board/core.h>
 #include <utils/function.h>
 #include <component/cpu.h>
+#include <fs/vfs/core.h>
+#include <fs/vfs/types.h>
+#include <fs/pseudofs.h>
+#include <fs/core.h>
 
 DEF_NOT_IMPL_FUNC(ni_set_irqs_enable, cpu_t *c, bool enabled);
+
+
+static int freq_read(fs_file_t *f, void *buf, size_t blen, uint32_t offset) {
+    cpu_t *cpu = f->data0;
+    char data[16];
+    int strlen = snprintf(data, sizeof(data), "%lu", (uint32_t) cpu->freq);
+    return pseudofs_write_to_buf(buf, blen, data, strlen + 1, offset);
+}
+
+static int schedid_read(fs_file_t *f, void *buf, size_t blen, uint32_t offset) {
+    cpu_t *cpu = f->data0;
+    char *id = ((component_t *) cpu->sched)->id;
+    return pseudofs_write_to_buf(buf, blen, id, COMPONENT_MAX_ID_LEN, offset);
+}
+
+static int create_cpu_sysfs(cpu_t *c) {
+    char buf[16];
+    snprintf(buf, sizeof(buf), "cpu%u", c->id);
+    fs_dentry_t *cpu_root = vfs_dentry_lookup_from(_laritos.fs.comp_root, "cpu");
+    fs_dentry_t *cpudir = vfs_dir_create(cpu_root, buf, FS_ACCESS_MODE_READ | FS_ACCESS_MODE_WRITE | FS_ACCESS_MODE_EXEC);
+    if (cpudir == NULL) {
+        error("Error creating '%s' sysfs directory", buf);
+        return -1;
+    }
+
+    if (pseudofs_create_custom_ro_file_with_dataptr(cpudir, "freq", freq_read, c) == NULL) {
+        error("Failed to create 'freq' sysfs file");
+        return -1;
+    }
+
+    if (pseudofs_create_custom_ro_file_with_dataptr(cpudir, "schedid", schedid_read, c) == NULL) {
+        error("Failed to create 'schedid' sysfs file");
+        return -1;
+    }
+    return 0;
+}
 
 int cpu_component_init(cpu_t *c, board_comp_t *bcomp,
         int (*init)(component_t *c), int (*deinit)(component_t *c)) {
@@ -49,5 +90,25 @@ int cpu_component_register(cpu_t *c) {
 
     // Save CPU shortcut
     _laritos.cpu[c->id] = (cpu_t *) c;
+
+    create_cpu_sysfs(c);
     return 0;
 }
+
+static int create_sysfs(sysfs_mod_t *sysfs) {
+    fs_dentry_t *cpu_root = vfs_dir_create(_laritos.fs.comp_root, "cpu",
+            FS_ACCESS_MODE_READ | FS_ACCESS_MODE_WRITE | FS_ACCESS_MODE_EXEC);
+    if (cpu_root == NULL) {
+        error("Error creating cpu sysfs directory");
+        return -1;
+    }
+
+    return 0;
+}
+
+static int remove_sysfs(sysfs_mod_t *sysfs) {
+    return vfs_dir_remove(_laritos.fs.comp_root, "cpu");
+}
+
+
+SYSFS_MODULE(cpu, create_sysfs, remove_sysfs)
