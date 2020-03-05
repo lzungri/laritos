0a3e77fa0c5e295c2e488678fbbe00d384c65dac
Leandro Zungri <lzungri@gmail.com>
2020-03-04 16:28:57 -0800

    property: Add property_t type

--- include/property/core.h
+++ include/property/core.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <dstruct/list.h>
 
 #define PROPERTY_ID_MAX_LEN 16
 #define PROPERTY_VALUE_MAX_LEN 128
@@ -12,6 +13,17 @@ typedef enum {
     PROPERTY_MODE_WRITE_BY_OWNER = 8,
 } prop_mode_t;
 
+struct pcb;
+typedef struct {
+    char id[PROPERTY_ID_MAX_LEN];
+    char value[PROPERTY_VALUE_MAX_LEN];
+    struct pcb *owner;
+    prop_mode_t mode;
+
+    list_head_t list;
+} property_t;
+
+int property_init_global_context(void);
 int property_create(char *id, prop_mode_t mode);
 int property_remove(char *id);
 int property_set(char *id, char *value);
