38050006ff5ab1e0faf2ea5a79cc57af1acf8a20
Leandro Zungri <lzungri@gmail.com>
2020-03-10 14:53:58 -0700

    init: Launch system processes from /sys/conf/init/launch_on_boot.conf

--- core/process/init.c
+++ core/process/init.c
@@ -1,5 +1,6 @@
 #include <log.h>
 
+#include <stdbool.h>
 #include <mm/heap.h>
 #include <process/core.h>
 #include <process/sysfs.h>
@@ -13,12 +14,103 @@
 #include <board/core.h>
 #include <utils/random.h>
 #include <utils/debug.h>
+#include <utils/symbol.h>
 #include <sched/core.h>
 #include <fs/vfs/core.h>
 #include <module/core.h>
 #include <generated/autoconf.h>
 #include <generated/utsrelease.h>
 
+
+static int launch_process(bool from_symbol, char *launcher) {
+    info("Launching process from %s '%s'", from_symbol ? "symbol" : "path", launcher);
+    if (from_symbol) {
+        int (*func)(void) = symbol_get(launcher);
+        if (func == NULL) {
+            error("No symbol '%s' found, check your launch_on_boot.conf file", launcher);
+            return -1;
+        }
+        return func();
+    }
+
+    return loader_load_executable_from_file(launcher) == NULL ? -1 : 0;
+}
+
+static int spawn_system_procs(void) {
+    info("Launching system processes");
+
+    fs_file_t *f = vfs_file_open("/sys/conf/init/launch_on_boot.conf", FS_ACCESS_MODE_READ);
+    if (f == NULL) {
+        error_async("Couldn't open kernel symbols file");
+        return -1;
+    }
+
+    int ret = 0;
+
+    // tokens: 0=K|U, 1=launcher
+    uint8_t token = 0;
+    uint8_t tokenpos = 0;
+    bool kernel;
+    char launcher[CONFIG_FS_MAX_FILENAME_LEN] = { 0 };
+    uint32_t offset = 0;
+    while (true) {
+        char buf[256];
+        int nbytes = vfs_file_read(f, buf, sizeof(buf), offset);
+        if (nbytes <= 0) {
+            break;
+        }
+
+        int i;
+        for (i = 0; i < nbytes; i++) {
+            if (buf[i] == '\n') {
+                // Make sure we have all the tokens, otherwise ignore the line
+                if (token != 1) {
+                    token = 0;
+                    tokenpos = 0;
+                    continue;
+                }
+
+                if (launch_process(kernel, launcher) < 0) {
+                    error("Couldn't launch %s process %s", kernel ? "kernel" : "user", launcher);
+                    ret = -1;
+                }
+
+                token = 0;
+                tokenpos = 0;
+                kernel = false;
+                memset(launcher, 0, sizeof(launcher));
+            } else if (buf[i] == ' ') {
+                token++;
+                tokenpos = 0;
+            } else {
+                if (token == 0) {
+                    if (tokenpos < 1) {
+                        kernel = buf[i] == 'K' || buf[i] == 'k';
+                        tokenpos++;
+                    }
+                } else if (token == 1) {
+                    if (tokenpos < sizeof(launcher) - 1) {
+                        launcher[tokenpos++] = buf[i];
+                    }
+                }
+            }
+        }
+
+        offset += nbytes;
+    }
+
+    // Launch last line process (in case there was no '\n')
+    if (token == 1) {
+        if (launch_process(kernel, launcher) < 0) {
+            error("Couldn't launch %s process %s", kernel ? "kernel" : "user", launcher);
+            ret = -1;
+        }
+    }
+
+    vfs_file_close(f);
+
+    return ret;
+}
 
 static int complete_init_process_setup(pcb_t *init) {
     init->parent = init;
@@ -92,7 +184,7 @@ int init_main(void *data) {
     // Seed random generator from current time
     random_seed((uint32_t) _laritos.timeinfo.boottime.secs);
 
-    assert(process_spawn_system_procs() >= 0, "Failed to create system processes");
+    assert(spawn_system_procs() >= 0, "Failed to create system processes");
 
     assert(ticker_start_all() >= 0, "Failed to start OS tickers");
 
