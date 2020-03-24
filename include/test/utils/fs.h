b5bfa9e4e7d902d42825dfd17a6c042a08f37ea7
Leandro Zungri <lzungri@gmail.com>
2020-03-23 17:33:45 -0700

    test: Fix typo

--- include/test/utils/fs.h
+++ include/test/utils/fs.h
@@ -8,6 +8,6 @@
 
 bool fs_file_in_listdir(char *dirpath, char *fname);
 
-static inline fs_dentry_t fs_get_data_testdir(void) {
+static inline fs_dentry_t *fs_get_data_testdir(void) {
     return vfs_dentry_lookup(DATA_TEST_DIR);
 }
