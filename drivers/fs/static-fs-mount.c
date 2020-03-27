9b5508494c3e7de6440f5783ba853e59c21a6d7a
Leandro Zungri <lzungri@gmail.com>
2020-03-26 19:44:06 -0700

    fs: Add mount mode component parameter

--- drivers/fs/static-fs-mount.c
+++ drivers/fs/static-fs-mount.c
@@ -1,5 +1,6 @@
 #include <log.h>
 
+#include <string.h>
 #include <board/core.h>
 #include <fs/vfs/core.h>
 #include <fs/vfs/types.h>
@@ -20,14 +21,26 @@ static int process(board_comp_t *comp) {
         return -1;
     }
 
+    char modestr[CONFIG_BOARD_INFO_MAX_TOKEN_LEN_BYTES];
+    if (board_get_str_attr(comp, "mode", modestr) < 0) {
+        error("No mode was specified in the board info for '%s'", comp->id);
+        return -1;
+    }
+    fs_access_mode_t mode = 0;
+    if (strchr(modestr, 'r') != NULL) {
+        mode |= FS_MOUNT_READ;
+    }
+    if (strchr(modestr, 'w') != NULL) {
+        mode |= FS_MOUNT_WRITE;
+    }
+
     blockdev_t *dev;
     if (board_get_component_attr(comp, "dev", (component_t **) &dev) < 0) {
         error("No dev was specified in the board info for '%s'", comp->id);
         return -1;
     }
 
-    fs_mount_t *mnt = vfs_mount_fs(type, mntpoint, FS_MOUNT_READ | FS_MOUNT_WRITE,
-            (fs_param_t []) { { "dev", dev }, { NULL } });
+    fs_mount_t *mnt = vfs_mount_fs(type, mntpoint, mode, (fs_param_t []) { { "dev", dev }, { NULL } });
     if (mnt == NULL) {
         error("Error mounting system image");
         return -1;
