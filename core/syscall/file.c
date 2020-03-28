becac62a2530ae5081f3280532143ad2336f35d2
Leandro Zungri <lzungri@gmail.com>
2020-03-28 02:01:07 -0700

    file: Create file if opened with write perms and doesn't exist

--- core/syscall/file.c
+++ core/syscall/file.c
@@ -10,6 +10,18 @@
 #include <fs/vfs/types.h>
 
 fs_file_t *syscall_open(char *path, fs_access_mode_t mode) {
+    // Create file if it was opened for writing and it doesn't exit
+    if ((mode & FS_ACCESS_MODE_WRITE) && !file_exist(path)) {
+        fs_dentry_t *parent = vfs_dentry_lookup_parent(path);
+        if (parent->inode->sb->fstype == vfs_get_fstype("pseudofs")) {
+            error("Permission denied: Cannot create files on pseudo file systems");
+            return NULL;
+        }
+        if (vfs_file_create(parent, file_get_basename(path), FS_ACCESS_MODE_READ | mode) == NULL) {
+            error("Could not create file '%s'", path);
+            return NULL;
+        }
+    }
     return vfs_file_open(path, mode);
 }
 
